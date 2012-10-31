1b1ae6ebf4cb78b6981ccad1bc2c1faf9392d990
Tom Evans <thomas.jude.evans@gmail.com>
2012-10-31 22:28:11 +0100

    Flight-details, new script to get flight info
    Started a new script which will be used to process GPS data to extract a
    list of flights, getting some information for these. For example
    duration, speed, direction, etc.

--- code_snippets.R
+++ code_snippets.R
@@ -1,4 +1,19 @@
-#*a place to store useful little code snippets
+#'a place to store useful little code snippets
 
-#get a list of available devices:
+#'get a list of available devices:
 devices <- sort(unique(gps$device_info_serial))
+
+
+
+#'Parallel function foreach is useful for where vectorised function is not possible.
+require(foreach)
+require(doParallel)
+cl <- makeCluster(parallel::detectCores())     #use x cores, general solution for any windows machine.
+registerDoParallel(cl)   #start the parellel session of R; the 'slaves', which will run the analysis.
+clusterExport(cl, c("x"))   #export vairables that will be needed in the 'slaves'.
+
+#mast a list object to recieve the data
+lst <- list()
+
+#close cluster
+stopCluster(cl)
---
+++ flight_details.R
@@ -0,0 +1,8 @@
+#'A script to process data on individual flights.
+#'It will summarise flights by distance travelled, altitude, speed etc.
+#'They will also be labelled by foraging trip and device_info_serial.
+#'The flights should be numbered for each foraging trip. Perhaps also labelling the first and final flight of a foraging trip - which may contitute 'commuting' flight.
+
+
+
+
