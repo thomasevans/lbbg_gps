40f378b5af3b75ab09f40a50358dba0bc4dceb1b
Tom Evans <thomas.jude.evans@gmail.com>
2012-11-19 17:47:37 +0100

    added weather script + finished draft flight para
    Finished flight_paramaters. Needs testing. Which is currently not
    possible, as have database connection problem.
    Will need to add some further database functions at the start.
    Started a new script in which weather data will be stored.

---
+++ weather_details.R
@@ -0,0 +1,4 @@
+#collect weather information, do for each GPS point (or flight?)
+#wind direction, speed
+#perhaps cloudcover, rain - look at what's available
+#then output to new table with device_info_serial and date_time for primary keys
