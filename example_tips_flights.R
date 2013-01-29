994c648a756cd0824d6b142dfd0c398b3312eae3
Tom Evans <thomas.jude.evans@gmail.com>
2013-01-29 23:07:43 +0100

    some edits and new script
    New script, this will make figures for example trips.
    Plan to make general, so that it will be possible to run the script for
    any specific trip (or a function perhaps), and get out nice illustrative
    figures (multi-panel figure probalby)

---
+++ example_tips_flights.R
@@ -0,0 +1,20 @@
+#Primarily developed by Tom Evans at Lund University: tom.evans@biol.lu.se
+#You are welcome to use parts of this code, but please give credit when using it extensively.
+
+
+#Description#######
+#This script produces figures and calculations, to show example foraging
+#With paramater values calculated etc.
+
+
+
+#Datbase functions#########
+
+
+
+
+
+#Ideas ####
+#Could use map thing, like did for GLS data before (then would
+#have coastlines etc.
+
--- flight_movement_calculations.R
+++ flight_movement_calculations.R
@@ -236,39 +236,65 @@ flights.characteristics <- cbind(flights.characteristics, wind.origin)
 # Values for Karlsö, according to current analysis appear a bit low.
 
 
-# Calculate angle between Track and Wind vector (alpha)
-# str(flights.characteristics)
-# flights.characteristics$wind.dir #wind bearing
-# flights$bearing_a_b  #flight bearing
+# Calculate angle between Track and Wind vector (beta)
 
-alpha  <- (flights.characteristics$wind.dir - flights$bearing_a_b)
-hist(alpha)
+#Difference between wind direction and flight direction
+beta  <- (flights.characteristics$wind.dir - flights$bearing_a_b)
+ hist(beta)
 
-alpha  <- abs(alpha)
-hist(alpha)
+# Absolute difference
+beta  <- abs(beta)
+ hist(beta)
 
 angle.dif <- function(x){
-  if(x > 180) {return (360 - x)}
-  else return (x)
+  #Function to find difference between flight and wind direction
+  #Range from 0 - 180
+  if(is.na(x)) return (NA)
+  else{
+    if(x > 180) {return (360 - x)}
+    else return (x)
+  }
 }
 
-test  <- sapply(alpha, angle.dif)
-
-
-hist(test)
-
-beta
-
-alpha <- (flights.characteristics$wind.10m.flt.ht * sin( 
-  ?sin
+angle.dif(NA)
 
 
 
+#Use 'angle.dif' function to calculate actual alpha value
+beta  <- sapply(beta, angle.dif)
+hist(beta)
 
+#y = (w.sin(b))/Va
+#Air speed
+Va <- 12
 
+y  <- (flights.characteristics$wind.10m.flt.ht * sin(rad(beta))) / Va
+y.rad <- y
+y.deg <- deg(y)
+alpha <- y.deg
+
+hist(y.deg)
+
+wind.vec <-  sapply(flights$bearing_a_b, angle.dif)
+hist(wind.vec)
+
+track.vec <-  sapply(flights.characteristics$wind.dir, angle.dif)
+hist(track.vec)
 
 
 # Output to database #####
+plot(alpha , track.vec)
+points(alpha, wind.vec, col = "red")
+reg1 <- lm(alpha ~ track.vec)
+reg2 <- lm(alpha ~ wind.vec)
+abline(reg1)
+abline(reg2, col = "red")
+
+
+#Inward flights
+plot(alpha[inward] , flights$bearing_a_b[inward])
+points(alpha[inward], beta[inward], col = "red")
+
 
 
 
