35ac25e32cb56672b9f08c27f59f4e27de996c53
Tom Evans <thomas.jude.evans@gmail.com>
2013-02-04 16:41:59 +0100

    Example trips extracting trip info and starting map
    Database data extraction and mapping of data

--- example_tips_flights.R
+++ example_tips_flights.R
@@ -41,21 +41,21 @@ str(trips)
 trips$start_time[1:10]
 tm <- as.POSIXlt(trips$start_time)
 #Check how this appears (i.e. time zone)
-tm[1:10]
+# tm[1:10]
 attr(tm,"tzone") <- "UTC"
 #Check how appears after change of time-zone - i.e. is the absolute time
 #value unchanged?
-tm[1:10]
+# tm[1:10]
 trips$start_time <- tm
 
 trips$end_time[1:10]
 tm <- as.POSIXlt(trips$end_time)
 #Check how this appears (i.e. time zone)
-tm[1:10]
+# tm[1:10]
 attr(tm,"tzone") <- "UTC"
 #Check how appears after change of time-zone - i.e. is the absolute time
 #value unchanged?
-tm[1:10]
+# tm[1:10]
 trips$end_time <- tm
 
 
@@ -66,42 +66,122 @@ trips$end_time <- tm
 trips.cond   <-  subset(trips, trips$interval_min < 800 &
                           trips$duration_s > (1.5 * 60 * 60))
 
-x <- sample(1:length(trips.cond), 5, replace=F)
-x <- c(x)
-x <- c(1, 5, 6, 8)
+names(trips.cond)
+length(trips.cond$trip_id)
 
-trips.cond[x]
-trips.cond[]
+#If we want repeatable results, we choose where to start our pseudo random series
+set.seed(1)
+
+x <- sample(1:length(trips.cond$trip_id), 5, replace=F)
+
+trips.sample <- trips.cond[x,]
+
+str(trips.sample)
+
+#Extract flights and gps points ######
+
+#For testing, get some initial values
+id     <-  4
+i      <-  trips.sample$device_info_serial[id]
+start.t  <-  trips.sample$start_time[id]
+end.t    <-  trips.sample$end_time[id]
+
+#Extract GPS points
+gps.extract <- function(i, start.t, end.t){
+#Function to extract required GPS data
+  q1a <- "SELECT DISTINCT g.device_info_serial, g.date_time, g.longitude,
+            g.latitude, g.x_speed, g.y_speed, g.z_speed, g.positiondop,
+            g.speed_accuracy, c.bearing_next, c.bearing_prev, c.nest_gc_dist,
+            c.nest_bear, c.inst_ground_speed, c.p2p_dist,  c.time_interval_s,
+            c.turning_angle, c.flight_class,  c.flight_id, g.altitude
+            FROM gps_uva_tracking_limited AS g, cal_mov_paramaters AS c
+            WHERE g.device_info_serial = c.device_info_serial
+            AND g.date_time = c.date_time
+            AND "
+
+  q1b <-  " ORDER BY g.device_info_serial ASC, g.date_time ASC ;"
+  
+
+  
+  q1c <- paste(" g.device_info_serial = ", i, " AND ",
+              "g.date_time >= #", start.t, 
+              "# AND g.date_time <= #", end.t, "# ", sep = "")
+  
+  
+  gps.sub <- sqlQuery(gps.db, query= gsub("\n", " ", paste(q1a, q1c, q1b, sep=""))
+                  ,as.is=TRUE)
+  
+  return(gps.sub)
+}
 
 
-str(trips)
+#Extract flight points
+flights.extract <- function(i, start.t, end.t){
+  #Function to extract required flights data
+  
+  #Parts of query
+  q1a <- "SELECT DISTINCT lf.*, lfc.*, lfw.*
+            FROM lund_flights AS lf, lund_flights_characteristics AS lfc,
+              lund_flights_weather AS lfw
+            WHERE lf.flight_id = lfc.flight_id AND  lf.flight_id = lfw.flight_id
+            AND "
+  
+  q1b <-  " ORDER BY lf.device_info_serial ASC, lf.flight_id ASC ;"
+  
+  
+  #Accepts the aguments given to the function, to extract the
+  #values requested.
+  q1c <- paste(" lf.device_info_serial = ", i, " AND ",
+               "lf.start_time >= #", start.t, 
+               "# AND lf.start_time <= #", end.t, "# ", sep = "")
+  
+  #Get flight information
+  flight.sub <- sqlQuery(gps.db, query= gsub("\n", " ", paste(q1a, q1c, q1b, sep=""))
+                      ,as.is=TRUE)  
+  drops <- c("flight_id.1","start_time.1", "device_info_serial.1", "flight_id.2", "start_time.2")
+  flight.sub <- flight.sub[,!(names(flight.sub) %in% drops)]
+
+  return(flight.sub)
+}
 
 
-hist(trips$interval_min)
+# test <- flights.extract(i, start.t, end.t)
+
+gps.sub <- gps.extract(i, start.t, end.t)
+flights.sub <- flights.extract(i, start.t, end.t)
 
 
+#Mapping trip #####
+
+#For testing, get some initial values
+id     <-  2
+i      <-  trips.sample$device_info_serial[id]
+start.t  <-  trips.sample$start_time[id]
+end.t    <-  trips.sample$end_time[id]
+
+gps.sub <- gps.extract(i, start.t, end.t)
+flights.sub <- flights.extract(i, start.t, end.t)
 
 
-q1a <- "SELECT DISTINCT g.device_info_serial, g.date_time, g.longitude,
-          g.latitude, g.x_speed, g.y_speed, g.z_speed, g.positiondop,
-          g.speed_accuracy, c.bearing_next, c.bearing_prev, c.nest_gc_dist,
-          c.nest_bear, c.inst_ground_speed, c.p2p_dist,  c.time_interval_s,
-          c.turning_angle, c.flight_class,  c.flight_id, g.altitude
-          FROM gps_uva_tracking_limited AS g, cal_mov_paramaters AS c
-          WHERE g.device_info_serial = c.device_info_serial
-          AND g.date_time = c.date_time
-          AND "
+# Map tracks ###########
+plot(gps.sub$longitude, gps.sub$latitude, col = as.numeric(gps.sub$flight_id))
 
-q1b <-  "ORDER BY g.device_info_serial ASC, g.date_time ASC ;"
+#Non-flight points
+points(gps.sub$longitude[gps.sub$flight_id == 0], gps.sub$latitude[gps.sub$flight_id == 0], col = "black")
 
-qc <- " g.device_info_serial = 519 " 
+#Then do dot-to-dot lines
+for( i in seq ( along = (unique(gps.sub$flight_id)))){
+  y <- unique(gps.sub$flight_id)[i]
+  
+  x <- subset(gps.sub, flight_id == y, select=c(longitude,latitude))
+  z <- length(x$longitude)
+  
+  segments(x$longitude[-1], x$latitude[-1], x$longitude[1:z-1], x$latitude[1:z-1],
+           col = as.numeric(y))
+}
 
-gps <- sqlQuery(gps.db, query= gsub("\n", " ", paste(q1a, qc, q1b, sep="")) ,as.is=TRUE)
-
-?sprintf
-
-
-
+names(gps.sub)
+unique(gps.sub$flight_class)
 
 #Ideas ####
 #Could use map thing, like did for GLS data before (then would
