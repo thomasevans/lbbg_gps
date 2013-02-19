8617957902803c74a229b399339991d0fbadb861
Tom Evans <thomas.jude.evans@gmail.com>
2013-02-19 15:23:16 +0100

    Small changes
    Should function the same, have generally tidied up the formatting

--- flight_details.R
+++ flight_details.R
@@ -13,7 +13,8 @@
 library(RODBC)
 
 #Establish a connection to the database
-gps.db <- odbcConnectAccess2007('F:/Documents/Work/GPS_DB/GPS_db.accdb')
+gps.db <- odbcConnectAccess2007(
+  'F:/Documents/Work/GPS_DB/GPS_db.accdb')
 
 #See what tables are available
 sqlTables(gps.db)
@@ -48,8 +49,7 @@ gps$date_time <- as.POSIXct(gps$date_time, tz="GMT",format="%Y-%m-%d %H:%M:%S")
 gps.original <- gps
 gps <- gps.original
 #for testing purposes we take the first x lines
-gps <- gps[1:50000,]
-
+# gps <- gps[1:50000,]
 
 # Recognise and label flight ####
 # Code to recognise flight, and adds this column,
@@ -96,11 +96,12 @@ gps$flight_class_2[ fly_type == 4 ]       <- 2
 
 # unique(gps$flight_class_2)
 
-
+# summary(as.factor(gps$flight_class_2))
 
 
 # Number flights ####
-#make column for flight id, start with value 0, which will be null value - i.e. not a trip (points at the nest)
+# Make column for flight id, start with value 0, which
+# will be null value - i.e. not a trip (points at the nest)
 gps$flight_id <- 0
 
 
@@ -109,89 +110,173 @@ gps$flight_id <- 0
 #d - device_info_serial
 #gps - the gps dataframe
 
-#summary(as.factor(gps$device_info_serial))
-#for testing
-#d <- 519
+# summary(as.factor(gps$device_info_serial))
+# for testing
+# d <- 519
+# flight.lab(519)
 
-flight.lab <- function(d, gps=get("gps", envir=environment(trip.lab))){
+flight.lab <- function(d, gps=get("gps", envir = environment(flight.lab))){
   
-  #first make a subset of 'gps' containing just data for device, d, away from the nest.
-  sub01 <- subset(gps, flight_class_2 != 0 & device_info_serial == d,select=c(flight_class_2,flight_id))
+  # First make a subset of 'gps' containing just data
+  # for device, d, away from the nest.
+  sub01 <- subset(gps, flight_class_2 != 0 &
+                    device_info_serial == d,
+                  select = c(flight_class_2, flight_id))
   
-  n <- length(sub01$flight_class_2) #the number of gps positions
+  #the number of gps positions
+  n <- length(sub01$flight_class_2) 
   
   
-  #a windows progress bar to monitor progress
-  pb <- winProgressBar(title = paste( "progress bar for device",d), min = 0,max = n, width = 300)
+  #A windows progress bar to monitor progress
+  pb <- winProgressBar(title = 
+                         paste( "progress bar for device", d),
+                       min = 0, max = n, width = 300)
   
-  x <- 0   #x will keep note of trip number, we start at zero.
-  #loop through all gps points, labelling them with trip id (x)
+  #x will keep note of trip number, we start at zero.
+  x <- 0   
+  y <- 0
+  
+  # loop through all gps points, labelling them with
+  # flight id (x)
   for(i in 1:n){
-    setWinProgressBar(pb, i, title=paste("device ", d, " is ", round(i/n*100, 0),"% done")) #refresh the progress bar, so that we can keep note of progress.
-    if(sub01$flight_class_2[i] == 1) x <- x+1      #if start of a trip, increment x by one
-    sub01$flight_id[i] <- x                    #allocated value of x for flight_id for position 'i'.
+    
+    # Refresh the progress bar, so that we can
+    # keep note of progress. Only run progress bar
+    # code every 1%, to avoid overly slowing things down.
+    if(y == floor(n/100)){
+    setWinProgressBar(pb, i, title = paste
+                      ("device ", d, " is ",
+                       round(i/n*100, 0),"% done"))
+    y <- 0
+    }
+    
+    # If start of a flight, increment x by one
+    if(sub01$flight_class_2[i] == 1) x <- x+1      
+    
+    # For progress bar
+    y <- y + 1
+    
+    # Allocated value of x for flight_id for position 'i'.
+    sub01$flight_id[i] <- x                    
   }
   
-  close(pb)    #close the windows progress bar
-  return(sub01$flight_id)            #output a vector for the bird of flight id
+  # Close the windows progress bar
+  close(pb)    
+  
+  # Output a vector for the bird of flight id
+  return(sub01$flight_id)   
+  
 }
-#**********End of this function: flight.lab
+# End of this function: flight.lab
 
 
 
+# Give each flight unique id. ####
+# Cribbed form trip_lab script
 
-#cribbed form trip_lab script
-
-#first make a list of available devices
+# First make a list of available devices
 devices <- sort(unique(gps$device_info_serial))
 
-#*Calculate trip id for each device
-#do this in parallel
-#first load neccessary packages
+# Calculate flight id for each device
+# do this in parallel
+# first load neccessary packages
 require(foreach)
 require(doParallel)
-cl <- makeCluster(parallel::detectCores())     #use x cores, general solution for any windows machine.
-registerDoParallel(cl)   #start the parellel session of R; the 'slaves', which will run the analysis.
 
-clusterExport(cl, c("devices", "gps"))   #this maybe neccessary so that the clustered instances or R have the required vairables/ functions in their scope, i.e. those functions and vairables which are referred to within the 'foreach' function.
+# Use x cores, general solution for any windows machine.
+cl <- makeCluster(parallel::detectCores())     
 
-#NB see: http://stackoverflow.com/questions/9404881/writing-to-global-variables-in-using-dosnow-and-doing-parallelization-in-r
-#There a solution is offered for exporting vairables from foreach to the global environment.
+# Start the parellel session of R; the 'slaves', which
+# will run the analysis.
+registerDoParallel(cl)
 
-#work out flight numbers for each device id, then add these to a list of lists (lst)
-#Use system.time to time how long this takes - so far has taken around 10-15 minutes on 8 thread machine.
-system.time({lst <- foreach(i = seq(along = devices )) %dopar%{
-  #calculate the trip numbers for the device i. i.e. the function which we wish to run for each device.     
-  x <- flight.lab(devices[i],gps)
-  list(x) #output x as list
-} #end of foreach functions
-}) #end of things being timed by system.time
+# Export required objects to slaves.
+# NB see: http://stackoverflow.com/questions/9404881/writing-to-global-variables-in-using-dosnow-and-doing-parallelization-in-r
+# There a solution is offered for exporting vairables
+# from foreach to the global environment.
+clusterExport(cl, c("devices", "gps"))
+
+
+# Work out flight numbers for each device id, then
+# add these to a list of lists (lst)
+# Use system.time to time how long this takes -
+# so far has taken around 10-15 minutes on 8 thread machine.
+system.time(
+  {lst <- foreach(i = seq(along = devices )) %dopar%{
+    
+  # Calculate the trip numbers for the device i.
+  # I.e. the function which we wish to run for each device.     
+  x <- flight.lab(devices[i], gps)
+  
+  # Output x as list
+  list(x) 
+
+  #end of foreach functions
+  } 
+   
+   #end of things being timed by system.time   
+}) 
 
 #close cluster
 stopCluster(cl)
 
-#make a vector to which the trip numbers will be added for all individuals.
-all.points <- rep(0,length(gps$device_info_serial))   #make this all zero (i.e. the default value - where there is no flight)
-z <- 0    #a vairable which will store the maximum flight number from the previous device
-x <- 0    #a new vector of flight numbers
 
-#Loop through all devices, making flight numbers unique, by adding number of highest numbered existing flight, then adding this to the all.points vector, this being filtered, so that only points in real trips get value.
+# A vector to which the trip numbers will be added
+# for all individuals.
+# Make this all zero (i.e. the default value - where
+# there is no flight).
+all.points <- rep(0, length(gps$device_info_serial))
+
+# To keep tab of the maximum flight number from
+# the previous device
+z <- 0
+
+# A new vector of flight numbers
+x <- 0    
+
+# For testing
+i <- 1
+
+# Loop through all devices, making flight numbers unique,
+# by adding number of highest numbered existing flight,
+# then adding this to the all.points vector, this being
+# filtered, so that only points in real trips get value.
 for(i in seq(along = devices )){
-  z <- max(x)    #highest current flight_id number
-  x <- unlist(lst[[i]]) + z   #add z to vector of current device trip numbers
-  d <- devices[i]       #device id
-  all.points[gps$flight_class_2 != 0 & (is.na(gps$flight_class_2) != TRUE) & gps$device_info_serial == d] <- x    #add vector x to the vector of all gps points
+  #highest current flight_id number
+  z <- max(x)    
+  
+  # Add z to vector of current device trip numbers
+  x <- unlist(lst[[i]]) + z  
+  
+  #device id
+  d <- devices[i]     
+  
+  # Add vector x to the vector of all gps points
+  all.points[gps$flight_class_2 != 0 &
+               (is.na(gps$flight_class_2) != TRUE)
+             & gps$device_info_serial == d]       <- x    
 }
 
 #then add the vector 'all.points' to the 'gps' dataframe.
 gps$flight_id <- all.points
 
+# gps$flight_id[1:2000]
 
 
-
-#now we have labelled all gps points with unique flight numbers we should now calculate various summary information for each flight.
-#This could either be done in the same script, or we could first add the flight number and flight status columns to the 'cal_mov_paramaters' DB table, then make a new script which queries this table and calculates flight information, then outputs this data to a new db table specifically for flight information.
-#To avoid over long and complicated scripts it is probably best to break this down, so first output the newly calculated column to the db, then start a new script for the flight calculations.
+# Now we have labelled all gps points with unique
+# flight numbers we should now calculate various
+# summary information for each flight.
+# This could either be done in the same script, or
+# we could first add the flight number and flight
+# status columns to the 'cal_mov_paramaters' DB 
+# table, then make a new script which queries this
+# table and calculates flight information, then
+# outputs this data to a new db table specifically
+# for flight information.
+# To avoid over long and complicated scripts it is
+# probably best to break this down, so first output
+# the newly calculated column to the db, then start
+# a new script for the flight calculations.
 
 
 # names(gps)
@@ -200,7 +285,8 @@ gps$flight_id <- all.points
 # gps$trip_id[1:100]
 # gps$flight_id[1:100]
 
-#output the new data columns to the 'cal_mov_paramaters' database table
+# Output the new data columns to the 'cal_mov_paramaters'
+#database table
 #add neccessary columns to db table first. Here 'flight_class' and 'flight_id', which are both integers.
 #put it all together in data_frame, with device_info_serial and date_time for primary keys.
 export_table <- as.data.frame(cbind(
@@ -214,9 +300,3 @@ export_table$date_time <- gps$date_time
 #first added the three new columns to the table useing access
 sqlUpdate(gps.db, export_table, tablename = "cal_mov_paramaters",index = c("device_info_serial","date_time"),fast=TRUE)
 
-
-# names(export_table)
-# 
-# odbcCloseAll()
-# 
-# ?RODBC
