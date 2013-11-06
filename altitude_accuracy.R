63afc92f8be83c876a5ccaf33800b5d3340d832f
Tom Evans <thomas.jude.evans@gmail.com>
2013-11-06 11:56:51 +0100

    New script looking at altitude accuracy to GPS data

---
+++ altitude_accuracy.R
@@ -0,0 +1,46 @@
+
+#Datbase functions#########
+#Get the flight data from the db.
+library(RODBC)
+
+#Establish a connection to the database
+gps.db <- odbcConnectAccess2007('D:/Documents/Work/GPS_DB/GPS_db.accdb')
+
+#See what tables are available
+#sqlTables(gps.db)
+
+
+#Get a copy of the flights DB table.
+alts <- sqlQuery(gps.db, query="SELECT DISTINCT g.v_accuracy, g.altitude, g.h_accuracy, g.positiondop
+FROM gps_uva_tracking_speed_3d_limited AS g
+                    ;",as.is=TRUE)
+
+
+
+
+str(alts)
+par(mfrow=c(1,1))
+sort(alts$v_accuracy)[1:100]
+rev(sort(alts$v_accuracy))[1:100]
+hist(alts$v_accuracy[alts$v_accuracy < 1000])
+hist(alts$v_accuracy[alts$v_accuracy < 100])
+hist(alts$v_accuracy[alts$v_accuracy < 50])
+sort((alts$altitude[alts$v_accuracy < 10]))[1:100]
+rev(sort((alts$altitude[alts$v_accuracy < 10])))[1:100]
+
+hist(alts$v_accuracy[alts$h_accuracy < 50])
+hist(alts$positiondop[alts$h_accuracy < 50])
+
+
+hist(alts$altitude[alts$v_accuracy < 20])
+sort(alts$altitude[alts$v_accuracy < 20])[1:100]
+rev(sort(alts$altitude[alts$v_accuracy < 20]))[1:100]
+
+summary(alts$v_accuracy < 30)
+
+min(alts$v_accuracy)
+
+
+
+
+
