b4d88886411f521499139adac5202cd9ba1abe00
Tom Evans <thomas.jude.evans@gmail.com>
2013-11-12 19:54:05 +0100

    Script to look at trips to Gotland

---
+++ gotland_trips.R
@@ -0,0 +1,23 @@
+
+
+
+
+
+
+#Datbase functions#########
+#Get the flight data from the db.
+library(RODBC)
+
+#Establish a connection to the database
+gps.db <- odbcConnectAccess2007('D:/Documents/Work/GPS_DB/GPS_db.accdb')
+
+#See what tables are available
+#sqlTables(gps.db)
+
+
+
+
+#Query the gull db to extract bird_id, nest_id, and nest locations
+trips <- sqlQuery(gps.db, query="SELECT DISTINCT t.*
+                  FROM lund_trips AS t
+                  ORDER BY t.trip_id ASC;")
