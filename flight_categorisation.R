d245287a2c88b776e28f4faebaff0a6dc504acad
Tom Evans <thomas.jude.evans@gmail.com>
2013-11-14 17:55:20 +0100

    Working to a point, but need to make foreach parallel loop and ouput to dataframe then database

--- flight_categorisation.R
+++ flight_categorisation.R
@@ -2,12 +2,7 @@
 # You are welcome to use parts of this code, but please give credit when using it extensively.
 # Code available at https://github.com/thomasevans/lbbg_gps
 
-# This script is to test different methods to categorise flight to sepperate out commuting (directed) flight from other types of flight (mainly expected to be searching).
-
-
-#'  1. Get a subset of flights to use in testing - get 50 of each
-#'   i.  For outward flights
-#'   ii. For inwward flights
+# This script produces a table of commuting flights with start and finish times, this will be based on a function to recognis when the flight ceases to be in 'commuting' phase.
 
 # Get data - database  ####
 
@@ -40,119 +35,126 @@ flights.all$end_time <- as.POSIXct(flights.all$end_time,
 flights.all$trip_flight_type <- as.factor(flights.all$trip_flight_type)
 # summary(flights.all$trip_flight_type)
 
-
 # Subsetting flight data ####
 flights.com <- subset(flights.all, trip_flight_type == "inward"  | trip_flight_type == "outward")
 
-# Fixed seed
-set.seed(25)
-# Sample of commuting flights
-fs <- sample(flights.com$flight_id,100)
+#testing script, take sample
+i <- sample(length(flights.com$trip_flight_type),100)
+flights.com <- flights.com[i,]
+
+# Index vector for flights.com
+flights.com.ind <- 1:length(flights.com$flight_id)
+
+i <- 5
+# for all flights    i in length flights.com
+data.all <- NULL    # initialisation
+# for(i in 1:length(flights.com.ind)){
+
+for(i in 1:10){
 
 # Get GPS points for these flights
 source("gps_extract.R")
 
-# Index vector for flights.com
-flights.com.ind <- 1:length(flights.com$flight_id)
-# i <- 5
-# Initialise 'points', which will hold all point data
-points <- NULL
-for( i in 1:length(fs)){
-  id  <- fs[i]
-  ind <- flights.com.ind[flights.com$flight_id == id]
-  points.temp <- gps.extract(flights.com$device_info_serial[ind],flights.com$start_time[ind],flights.com$end_time[ind])
-  points <- rbind(points,points.temp)
-}
+id <- flights.com$flight_id[i]
 
+
+# Get GPS points for flight
+points <- gps.extract(flights.com$device_info_serial[i],flights.com$start_time[i],flights.com$end_time[i])
+
+# Correct data_time format
 points$date_time <- as.POSIXct(points$date_time,
                                    tz="GMT",
                                    format="%Y-%m-%d %H:%M:%S")
 
-#'  2. Plot maps for non categorised outward and inward flights
 
-# Map non-categorised flights ####
-# Get mapping function
-source("maps_flights.R")
-
-# map specified
-maps.flights(points, all.flights = FALSE, flight.id = fs[20:25])
-
-maps.flights(points, all.flights = TRUE)
-
-
-#'  3. Make categorisation alogrithm
-
-# Categorisation testing ####
-ft <- fs[c(27, 10, 36, 41)]
-c(27, 10, 36, 41)
-maps.flights(points, all.flights = FALSE, flight.id = ft)
-maps.flights(points, all.flights = FALSE, flight.id = fs[10])
-
-x <- c(1,2,3,NULL,NULL)
-x <- NULL
-mean(x)
-str(points)
-
-# 10
-
-# Subset flight data
-f.points <- subset(points,flight_id == fs[27])
-n <- length(f.points$flight_id)
-id <- fs[27]
-str(flights.com)
+# Categorisation ####
 
 # Get direction - outward or inward
 dir <- 1
-if(flights.com$trip_flight_type[flights.com$flight_id == id] == "inward") dir <- -1
+if(flights.com$trip_flight_type[i] == "inward") dir <- -1
 
-# Calculate speed relative to displacement from island
-d.dist <- f.points$nest_gc_dist[2:n] - f.points$nest_gc_dist[1:(n-1)]
-d.dist <- d.dist *1000   # Get to metres
-d.speed <- d.dist/ f.points$time_interval_s[2:n]
-d.speed.cor <- d.speed*dir
-d.speed.cor <- c(0,d.speed.cor) #add a value for first point
+n <- length(points$device_info_serial)
 
-# Change in speed relative to island relative to last 3 points
-# Reverse point list if inward flight (work from island out)
-if(dir == -1) {
-  ds <- rev(d.speed.cor)} else {
-    ds <- d.speed.cor
+  if(n < 5){
+    data.flight <- cbind(flights.com$trip_id[i],points$device_info_serial[1],flights.com$start_time[i],flights.com$end_time[i])
+    data.all <- rbind(data.all,data.flight)
+  } else {
+    
+    # Calculate speed relative to displacement from island
+    d.dist <- points$nest_gc_dist[2:n] - points$nest_gc_dist[1:(n-1)]
+    d.dist <- d.dist *1000   # Get to metres
+    d.speed <- d.dist/ points$time_interval_s[2:n]
+    d.speed.cor <- d.speed*dir
+    d.speed.cor <- c(0,d.speed.cor) #add a value for first point
+    
+    # Change in speed relative to island relative to last 3 points
+    # Reverse point list if inward flight (work from island out)
+    if(dir == -1) {
+      ds <- rev(d.speed.cor)} else {
+        ds <- d.speed.cor
+      }
+    
+    #Change in speed from previous points
+    d.dif <- function(i, ds = ds){
+      ds[i]/mean(ds[(i-1):(i-3)])
+    }
+    
+    #apply function
+    x <- sapply(c(4:length(ds)),d.dif,ds=ds) 
+    x <- c(1,1,1,x)   #include first 3 points
+    
+    s <- TRUE
+    p.stop <- NULL
+    for(it in 1:length(x)){
+      if(x[it] < 0.2 & s){
+        s <- FALSE
+        p.stop <- it
+      }
+    }
+    # i
+    
+    
+    if(dir == 1) {
+        # If outward
+        time.start <- points$date_time[1]
+        if(is.null(p.stop)){
+          time.end <- points$date_time[n]
+        } else time.end <- points$date_time[p.stop]
+    }else{    
+        # If inward (need to reverse order again)
+        time.end <- points$date_time[n]
+        if(is.null(p.stop)){
+          time.start <- points$date_time[1]
+        } else {
+            time.start <- points$date_time[n - p.stop]
+          } 
+    }
+    
+    
+    data.flight <- cbind(id,points$device_info_serial[1],time.start,time.end)
+    data.all <- rbind(data.all,data.flight)
   }
 
-#Change in speed from previous points
-d.dif <- function(i, ds = ds){
-  ds[i]/mean(ds[(i-1):(i-3)])
 }
 
-#apply function
-x <- sapply(c(4:length(ds)),d.dif,ds=ds) 
-x <- c(1,1,1,x)   #include first 3 points
+i
 
-s <- TRUE
-p.stop <- NULL
-for(i in 1:length(x)){
-  if(x[i] < 0.2 & s){
-    s <- FALSE
-    p.stop <- i
-  }
-}
-
+# x[it]
+# x
+# 
+# p.ind <- rep(FALSE,length(f.points$nest_gc_dist))
+# p.ind[1:p.stop] <- TRUE
+# p.ind <- rev(p.ind)
+# plot(f.points$latitude, f.points$longitude)
+# points(f.points$latitude[p.ind], f.points$longitude[p.ind], col = "red")
+# 
+# 
+# plot(x, ylim = c(-2,2))
 # plot(rev(f.points$nest_gc_dist))
-# points(rev(f.points$nest_gc_dist)[1:p.stop], col = "red")
-
-p.ind <- rep(FALSE,length(f.points$nest_gc_dist))
-p.ind[1:p.stop] <- TRUE
-p.ind <- rev(p.ind)
-plot(f.points$latitude, f.points$longitude)
-points(f.points$latitude[p.ind], f.points$longitude[p.ind], col = "red")
-
-
-plot(x, ylim = c(-2,2))
-plot(rev(f.points$nest_gc_dist))
-
-plot(d.speed.cor)
-time_interval_s
-nest_gc_dist
+# 
+# plot(d.speed.cor)
+# time_interval_s
+# nest_gc_dist
 
 
 
