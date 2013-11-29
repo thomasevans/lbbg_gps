8dc7c9bab8b88c1326ca1cbeed22d74c5f554235
Tom Evans <thomas.jude.evans@gmail.com>
2013-11-29 15:47:03 +0100

    Added code to deal with NA values - return NAs if NA in else make calculation

--- wind_dir_speed.R
+++ wind_dir_speed.R
@@ -10,28 +10,31 @@ wind.dir.speed <- function(uwind10, vwind10){
   # This function calculates the wind speed and direction based on the u
   # v wind vectors
   
-  #Wind speed Pythagoras theorem
-  wind.speed <- sqrt((uwind10 * uwind10) + (vwind10 * vwind10))
-  
-  # Calculate direction in radians (0 - 90 deg)
-  dir <- atan(abs(uwind10/ vwind10))
-  
-  #   atan(1)
-  #   atan(0.5)
-  #   dir <- atan(0.5)
-  #   ?atan
-  # Direction in degrees (0 - 90)
-  dir <- dir * 180 / pi
-  
-  # Make into bearing from North
-  if(uwind10 > 0 && vwind10 < 0){
-    wind.dir <- (180 - dir)
-  }else if(uwind10 < 0 && vwind10 < 0){
-    wind.dir <- (dir + 180)
-  }else if(uwind10 < 0 && vwind10 > 0){
-    wind.dir <- (360 - dir)
-  }else   wind.dir <- (dir)
-  
-  x <- cbind(wind.speed, wind.dir)
-  return(x)
+  if(is.na(uwind10) | is.na(vwind10)) return(t(c(NA,NA))) else {
+    
+    #Wind speed Pythagoras theorem
+    wind.speed <- sqrt((uwind10 * uwind10) + (vwind10 * vwind10))
+    
+    # Calculate direction in radians (0 - 90 deg)
+    dir <- atan(abs(uwind10/ vwind10))
+    
+    #   atan(1)
+    #   atan(0.5)
+    #   dir <- atan(0.5)
+    #   ?atan
+    # Direction in degrees (0 - 90)
+    dir <- dir * 180 / pi
+    
+    # Make into bearing from North
+    if(uwind10 > 0 && vwind10 < 0){
+      wind.dir <- (180 - dir)
+    }else if(uwind10 < 0 && vwind10 < 0){
+      wind.dir <- (dir + 180)
+    }else if(uwind10 < 0 && vwind10 > 0){
+      wind.dir <- (360 - dir)
+    }else   wind.dir <- (dir)
+    
+    x <- cbind(wind.speed, wind.dir)
+    return(x)
+  }
 }
