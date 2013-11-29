eefa24df0aec89ecd745c6339d65786563fb1ca3
Tom Evans <thomas.jude.evans@gmail.com>
2013-11-29 14:41:38 +0100

    Function from 'flights_drift_calculation_example.R' to calculate wind direction and speed from u and v wind speed components

---
+++ wind_dir_speed.R
@@ -0,0 +1,37 @@
+# Developed by Tom Evans at Lund University: tom.evans@biol.lu.se
+# You are welcome to use parts of this code, but please give credit when using it extensively.
+# Code available at https://github.com/thomasevans/lbbg_gps
+
+# Wind shear -------
+# Calculate wind direction and speed from u and v wind components
+
+
+wind.dir.speed <- function(uwind10, vwind10){
+  # This function calculates the wind speed and direction based on the u
+  # v wind vectors
+  
+  #Wind speed Pythagoras theorem
+  wind.speed <- sqrt((uwind10 * uwind10) + (vwind10 * vwind10))
+  
+  # Calculate direction in radians (0 - 90 deg)
+  dir <- atan(abs(uwind10/ vwind10))
+  
+  #   atan(1)
+  #   atan(0.5)
+  #   dir <- atan(0.5)
+  #   ?atan
+  # Direction in degrees (0 - 90)
+  dir <- dir * 180 / pi
+  
+  # Make into bearing from North
+  if(uwind10 > 0 && vwind10 < 0){
+    wind.dir <- (180 - dir)
+  }else if(uwind10 < 0 && vwind10 < 0){
+    wind.dir <- (dir + 180)
+  }else if(uwind10 < 0 && vwind10 > 0){
+    wind.dir <- (360 - dir)
+  }else   wind.dir <- (dir)
+  
+  x <- cbind(wind.speed, wind.dir)
+  return(x)
+}
