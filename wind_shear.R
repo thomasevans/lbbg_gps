f314533c19096342a65d7eab2965b2f43d06def8
Tom Evans <thomas.jude.evans@gmail.com>
2013-11-29 14:41:03 +0100

    Added code attribution

--- wind_shear.R
+++ wind_shear.R
@@ -1,3 +1,9 @@
+# Developed by Tom Evans at Lund University: tom.evans@biol.lu.se
+# You are welcome to use parts of this code, but please give credit when using it extensively.
+# Code available at https://github.com/thomasevans/lbbg_gps
+
+
+
 # Wind shear -------
 
 # Making aproximate calculation of wind speed at median flight height
