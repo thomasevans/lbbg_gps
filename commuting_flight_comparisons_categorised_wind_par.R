14ee8c3f8614b2873b94437bee04bc27db11f1dc
Tom Evans <thomas.jude.evans@gmail.com>
2013-12-01 14:21:48 +0100

    Calculating summary statistics

--- commuting_flight_comparisons_categorised_wind_par.R
+++ commuting_flight_comparisons_categorised_wind_par.R
@@ -1,9 +1,141 @@
+# Primarily developed by Tom Evans at Lund University: tom.evans@biol.lu.se
+# You are welcome to use parts of this code, but please give credit when using it extensively.
+
+
 # Summarise info from 'lund_flight_points_wind_par' for commuting flight classification
 
-# Get commuting flight details (start time, end time etc)
 
-# Get lund_flight_points_wind_par dataframe
 
-# For each flight do following:
-# - see paper notes
+#Datbase functions#########
+#Get the flight data from the db.
+library(RODBC)
+
+#Establish a connection to the database
+gps.db <- odbcConnectAccess2007('D:/Documents/Work/GPS_DB/GPS_db.accdb')
+
+#See what tables are available
+#sqlTables(gps.db)
+
+
+#Get a copy of the flights DB table.
+flights <- sqlQuery(gps.db, query = "SELECT DISTINCT f.*
+                    FROM lund_flights_commuting AS f
+                    ORDER BY f.flight_id ASC;")
+# str(flights)
+
+#Get a copy of the lund_flight_points_wind_par DB table.
+points_par <- sqlQuery(gps.db, query = "SELECT DISTINCT f.*
+                    FROM lund_flight_points_wind_par AS f
+                    ORDER BY f.flight_id ASC;")
+# str(points_par)
+
+
+# Calculate summary statistics -----
+
+# i <- 5
+
+
+
+get.stats <- function(i, points_par = points_par, flights = flights){
+  # Get flight points
+  require(CircStats)
+  sub.points <- subset(points_par,
+                       device_info_serial == flights$device_info_serial[i] &
+                       date_time >= flights$start_time[i] &
+                       date_time <= flights$end_time[i] &
+                       head_speed > 5 &
+                        ground_speed > 2   
+                         )
+  # str(points_par)
+  
+  # sub.points <- sub.points
+  
+  # calculate paramaters:
+  wind_side_mean <- mean(sub.points$wind_side, na.rm = TRUE)
+  wind_side_median <- median(sub.points$wind_side, na.rm = TRUE)
+  
+  wind_head_tail_mean <- mean(sub.points$wind_head_tail, na.rm = TRUE)
+  wind_head_tail_median <- median(sub.points$wind_head_tail, na.rm = TRUE)
+  
+  wind_sc_mean <- mean(sub.points$wind_sc, na.rm = TRUE)
+  wind_sc_median <- median(sub.points$wind_sc, na.rm = TRUE)
+  
+  alt_new_mean <- mean(sub.points$altnew, na.rm = TRUE)
+  alt_new_median <- median(sub.points$altnew, na.rm = TRUE)
+  
+  vignette("CircStats")
+  
+  alpha_mean <- deg(circ.mean(rad(sub.points$alpha)))
+  alpha_rho <- est.rho(rad(sub.points$alpha))
+  
+  beta_mean <- deg(circ.mean(rad(sub.points$beta)))
+  beta_median <- est.rho(rad(sub.points$beta))
+  
+  wind_dir_track_mean <- deg(circ.mean(rad(sub.points$wind_dir_track)))
+  wind_dir_track_median <- est.rho(rad(sub.points$wind_dir_track))
+  wind_dir_track_circvar <- circ.disp(rad(sub.points$wind_dir_track))[,4]
+  
+  wind_dir_deg_mean <- deg(circ.mean(rad(sub.points$wind_dir_deg)))
+  wind_dir_deg_median <- est.rho(rad(sub.points$wind_dir_deg))
+  
+  flight_id <- flights$flight_id[i]
+  
+  calc.par <- c(flights$flight_id[i],
+                wind_side_mean, wind_side_median,
+                wind_head_tail_mean, wind_head_tail_median,
+                wind_sc_mean, wind_sc_median,
+                alt_new_mean, alt_new_median,
+                alpha_mean, alpha_rho, beta_mean,
+                beta_median, wind_dir_track_mean,
+                wind_dir_track_median,
+                wind_dir_track_circvar,
+                wind_dir_deg_mean,
+                wind_dir_deg_median)
+  
+  return(calc.par)
+}
+
+
+require(foreach)
+require(doParallel)
+
+#Make cluster of number of devices instances
+cl <- makeCluster(detectCores())
+# ?makeCluster
+
+#start the parellel session of R; the 'slaves', which will run the analysis.
+registerDoParallel(cl)  
+
+#export needed data
+clusterExport(cl, c("flights","points_par","get.stats"))  
+
+#make a list object to recieve the data
+lst <- list()
+f <- length(flights$flight_id)
+system.time({lst <- foreach(i = 1:100 ) %dopar%{
+  get.stats(i, flights = flights, points_par = points_par)
+} #end of foreach functions
+}) #end of things being timed by system.time
+
+#close cluster
+stopCluster(cl)
+
+# Merge to dataframe
+flights.par <- do.call(rbind , lst)
+names(flights.par) <- c("flight_id", "
+                        wind_side_mean", " wind_side_median", "
+                        wind_head_tail_mean", " wind_head_tail_median", "
+                        wind_sc_mean", " wind_sc_median", "
+                        alt_new_mean", " alt_new_median", "
+                        alpha_mean", " alpha_rho", " beta_mean", "
+                        beta_median", " wind_dir_track_mean", "
+                        wind_dir_track_median", "
+                        wind_dir_track_circvar", "
+                        wind_dir_deg_mean", "
+                        wind_dir_deg_median")
+
+head(flights.par)
+str(flights.par)
+
+
 
