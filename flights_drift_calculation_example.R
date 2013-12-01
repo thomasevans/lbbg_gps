baf4d9b5d42a2f536e1a566949503ccd64d51f33
Tom Evans <thomas.jude.evans@gmail.com>
2013-12-01 11:40:04 +0100

    Text on who developed code

--- flights_drift_calculation_example.R
+++ flights_drift_calculation_example.R
@@ -1,3 +1,9 @@
+# Developed by Tom Evans at Lund University: tom.evans@biol.lu.se
+# You are welcome to use parts of this code, but please give credit when using it extensively.
+# Code available at https://github.com/thomasevans/lbbg_gps
+
+
+
 #' Scratchpad for thinking about analysis of wind drift/ wind effect
 #' 
 #' Plan to include:
