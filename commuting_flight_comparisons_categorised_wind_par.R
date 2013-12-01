90093093dad6e2b9533f6ea78a80d9f3c41b2236
Tom Evans <thomas.jude.evans@gmail.com>
2013-12-01 11:40:33 +0100

    New script to get summary statistics for each commutting flight for wind paramaters

---
+++ commuting_flight_comparisons_categorised_wind_par.R
@@ -0,0 +1,9 @@
+# Summarise info from 'lund_flight_points_wind_par' for commuting flight classification
+
+# Get commuting flight details (start time, end time etc)
+
+# Get lund_flight_points_wind_par dataframe
+
+# For each flight do following:
+# - see paper notes
+
