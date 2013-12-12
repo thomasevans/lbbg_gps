54554ac6ffafaf058b41ca6f5436928995261052
Tom Evans <thomas.jude.evans@gmail.com>
2013-12-12 13:44:05 +0100

    Added option to just get GPS locations (device_info_serial, lat, long, datetime)

--- gps_extract.R
+++ gps_extract.R
@@ -8,12 +8,13 @@
 
 
 #Extract GPS points
-gps.extract <- function(i, start.t, end.t, weather = FALSE, DB = FALSE){
+gps.extract <- function(i, start.t, end.t, weather = FALSE, simple = FALSE, DB = FALSE){
 
   gc()
   # i - device info serial
   # start.t - first point date-time
   # end.t - final point date_time
+  # simple - if TRUE only return device_info_serial, lat, long, date_time
   #Function to extract required GPS data
   
   require(RODBC)
@@ -31,30 +32,37 @@ gps.extract <- function(i, start.t, end.t, weather = FALSE, DB = FALSE){
 
   } else gps.db2 <- DB
   
-  if(weather == FALSE) {q1a <- "SELECT DISTINCT g.device_info_serial, g.date_time, g.longitude,
-  g.latitude, g.positiondop, g.speed_accuracy, g.vnorth,
-  g.veast, g.vdown, g.speed_3d, g.h_accuracy, g.v_accuracy, c.bearing_next,
-  c.bearing_prev, c.nest_gc_dist, c.calculated_speed,
-  c.nest_bear, c.inst_ground_speed, c.p2p_dist,  c.time_interval_s,
-  c.turning_angle, c.flight_class,  c.flight_id, g.altitude
-  FROM gps_uva_tracking_speed_3d_limited AS g, lund_gps_parameters AS c
-  WHERE g.device_info_serial = c.device_info_serial
-  AND g.date_time = c.date_time
-  AND "} else {
-    q1a <- "SELECT DISTINCT g.device_info_serial, g.date_time, g.longitude,
-  g.latitude, g.positiondop, g.speed_accuracy, g.vnorth,
-  g.veast, g.vdown, g.speed_3d, g.h_accuracy, g.v_accuracy, c.bearing_next,
-  c.bearing_prev, c.nest_gc_dist, c.calculated_speed,
-  c.nest_bear, c.inst_ground_speed, c.p2p_dist,  c.time_interval_s,
-  c.turning_angle, c.flight_class,  c.flight_id, g.altitude, lw.uwnd_10m,
-  lw.vwnd_10m
-  FROM gps_uva_tracking_speed_3d_limited AS g, lund_gps_parameters AS c, lund_points_weather as lw
-  WHERE g.device_info_serial = c.device_info_serial
-  AND g.date_time = c.date_time
-  AND g.device_info_serial = lw.device_info_serial
-  AND g.date_time = lw.date_time
-  AND "
-  }
+    if(simple == FALSE){
+      if(weather == FALSE) {q1a <- "SELECT DISTINCT g.device_info_serial, g.date_time, g.longitude,
+      g.latitude, g.positiondop, g.speed_accuracy, g.vnorth,
+      g.veast, g.vdown, g.speed_3d, g.h_accuracy, g.v_accuracy, c.bearing_next,
+      c.bearing_prev, c.nest_gc_dist, c.calculated_speed,
+      c.nest_bear, c.inst_ground_speed, c.p2p_dist,  c.time_interval_s,
+      c.turning_angle, c.flight_class,  c.flight_id, g.altitude
+      FROM gps_uva_tracking_speed_3d_limited AS g, lund_gps_parameters AS c
+      WHERE g.device_info_serial = c.device_info_serial
+      AND g.date_time = c.date_time
+      AND "} else {
+        q1a <- "SELECT DISTINCT g.device_info_serial, g.date_time, g.longitude,
+      g.latitude, g.positiondop, g.speed_accuracy, g.vnorth,
+      g.veast, g.vdown, g.speed_3d, g.h_accuracy, g.v_accuracy, c.bearing_next,
+      c.bearing_prev, c.nest_gc_dist, c.calculated_speed,
+      c.nest_bear, c.inst_ground_speed, c.p2p_dist,  c.time_interval_s,
+      c.turning_angle, c.flight_class,  c.flight_id, g.altitude, lw.uwnd_10m,
+      lw.vwnd_10m
+      FROM gps_uva_tracking_speed_3d_limited AS g, lund_gps_parameters AS c, lund_points_weather as lw
+      WHERE g.device_info_serial = c.device_info_serial
+      AND g.date_time = c.date_time
+      AND g.device_info_serial = lw.device_info_serial
+      AND g.date_time = lw.date_time
+      AND "
+      }
+    } else {
+      q1a <- "SELECT DISTINCT g.device_info_serial, g.date_time, g.longitude,
+      g.latitude
+      FROM gps_uva_tracking_speed_3d_limited AS g
+      WHERE "      
+    }
 
   q1b <-  " ORDER BY g.device_info_serial ASC, g.date_time ASC ;"
   
