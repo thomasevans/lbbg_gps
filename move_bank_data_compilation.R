a05a8cea55fa2deab06a366bfaa8d6720df833cc
Tom Evans <thomas.jude.evans@gmail.com>
2014-01-07 10:40:16 +0100

    New script to combine extracted variables from MoveBank

---
+++ move_bank_data_compilation.R
@@ -0,0 +1,8 @@
+#Primarily developed by Tom Evans at Lund University: tom.evans@biol.lu.se
+#You are welcome to use parts of this code, but please give credit when using it extensively.
+
+#' Data from MoveBank, extracted as sepperate files for each vairable
+#' Here we combine all the extracted variables and output to a 
+#' csv file, this will then be imported into a new Access DB table.
+
+
