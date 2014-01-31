23894cd4da1c67d8f6b11cead446602ee19b03c7
Tom Evans <thomas.jude.evans@gmail.com>
2014-01-31 17:02:13 +0100

    New script, largely based on 'commuting_flight_comparisons_categorised_ecmwf' where flights are statistically analysed (LMMs) for flight altitude etc.

---
+++ commuting_flight_comparisons_categorised_stats.R
@@ -0,0 +1,10 @@
+#Primarily developed by Tom Evans at Lund University: tom.evans@biol.lu.se
+#You are welcome to use parts of this code, but please give credit when using it extensively.
+
+#Description ----
+# In this script we analyse flights to look at flight altitude, airspeed, and straightness, and how these are effected by different conditions, e.g. winds. Further we analyse flights with respect to wind drift - do the gulls compensate for this?
+
+# Alternative working directory for when also running another script from same directory.
+# setwd("D:/Dropbox/R_projects/lbbg_gps/workspace_alternative")
+
+
