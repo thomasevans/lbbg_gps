13e881bb7f7b21be380febde5e5c7db8be7c86fa
Tom Evans <thomas.jude.evans@gmail.com>
2014-02-01 22:35:51 +0100

    Getting data and processing pre-analysis

--- commuting_flight_comparisons_categorised_stats.R
+++ commuting_flight_comparisons_categorised_stats.R
@@ -8,3 +8,219 @@
 # setwd("D:/Dropbox/R_projects/lbbg_gps/workspace_alternative")
 
 
+
+#Datbase functions#########
+#Get the flight data from the db.
+library(RODBC)
+
+#Establish a connection to the database
+gps.db <- odbcConnectAccess2007('D:/Documents/Work/GPS_DB/GPS_db.accdb')
+
+#See what tables are available
+#sqlTables(gps.db)
+
+
+#Get a copy of the flights DB table.
+# WHOLE FLIGHT
+flights <- sqlQuery(gps.db, query="SELECT DISTINCT f.*, l.trip_flight_n,l.trip_id, l.trip_flight_type, w.* 
+                    FROM lund_flights_commuting_par AS f, lund_flights as l, lund_flight_com_wind_par_ecmwf AS w
+                    WHERE f.flight_id = l.flight_id
+                    AND f.flight_id = w.flight_id
+                    ORDER BY f.flight_id ASC;")
+
+
+# Get flight weather data
+# Assemble SQL request
+q1 <- "SELECT DISTINCT f.*
+FROM lund_flights_weather AS f
+WHERE f.flight_id IN ("
+
+fun <- function(x){
+  x <- as.numeric(as.character(x))
+  paste(" ", x, ",", sep = "")
+}
+
+q2 <- paste(sapply(flights$flight_id,fun), collapse = "")                            
+
+q3 <- ")
+ORDER BY f.flight_id ASC;"
+
+#Get a copy of the flights_weather DB table.
+flights.weather <- sqlQuery(gps.db, query =
+                              gsub("\n", " ",
+                                   paste(q1, q2, q3, sep="")),
+                            as.is = TRUE)
+
+# Hack to set time zone back to UTC rather than system locale.
+# See: http://stackoverflow.com/questions/7484880/how-to-read-utc-timestamps-from-sql-server-using-rodbc-in-r
+tm <- NULL
+tm <- as.POSIXlt(flights.weather$start_time)
+#Check how this appears (i.e. time zone)
+# head(tm)
+attr(tm, "tzone") <- "UTC"
+#Check how appears after change of time-zone - i.e. is the absolute time
+#value unchanged?
+# head(tm)
+flights.weather$start_time <- tm
+
+
+
+#Query the gull db to extract trip information
+trips <- sqlQuery(gps.db, query="SELECT DISTINCT t.*
+                  FROM lund_trips AS t
+                  ORDER BY t.trip_id ASC;")
+# str(trips)
+
+# Get flight information
+flights.characteristics <- sqlQuery(gps.db, query="SELECT DISTINCT f.*
+                                    FROM lund_flight_com_weather_par AS f
+                                    ORDER BY f.flight_id ASC;")
+
+# str(flights.characteristics)
+
+
+
+# Label trip type and duration -----
+trip_type     <- 0
+trip_duration <- 0
+trip_gotland  <- 0
+trip_distmax  <- 0
+
+# Go through all trips, labelling by trip type duration etc.
+# Later to be used for filtering criteria
+for(i in seq(along = flights$trip_id)){
+  trip_type[i] <- trips$trip_type[trips$trip_id ==
+                                    flights$trip_id[i]][1]
+  trip_duration[i] <- trips$duration_s[trips$trip_id ==
+                                         flights$trip_id[i]][1]
+  trip_gotland[i] <- trips$gotland[trips$trip_id ==
+                                     flights$trip_id[i]][1]
+  trip_distmax[i] <- trips$dist_max[trips$trip_id ==
+                                      flights$trip_id[i]][1]
+}
+
+# Make to factors - not numeric
+trip_type    <- as.factor(trip_type)
+trip_gotland <- as.factor(trip_gotland)
+# 
+# summary(trip_type)
+# summary(trip_gotland)
+
+
+# Sepparate outward and inward flights ------
+outward <- (flights$trip_flight_type == "outward") & trip_gotland == 0 & (flights$interval_mean < 800) & (trip_distmax > 4) & (trip_distmax < 400) & flights$points > 4 & flights$dist_a_b > 2000
+
+inward  <- (flights$trip_flight_type == "inward")  & (trip_gotland == 0) & (flights$interval_mean < 800) & (trip_distmax > 4) & (trip_distmax < 400) & flights$points > 4  & flights$dist_a_b > 2000
+
+summary(inward)
+summary(outward)
+
+
+# Angle with respect to wind
+dif.angle <- flights$bearing_a_b - flights.characteristics$winddir
+# hist(dif.angle)
+dif.angle <- abs(dif.angle)
+
+cor.ang <- function(x){
+  if(x > 180){ y <- 180 - (x - 180)}
+  else y <- x
+  return(y)
+}
+# cor.ang(181)
+dif.angle <- sapply(dif.angle, cor.ang)
+#  hist(dif.angle)
+
+
+# Re-arrange the data for paired data comparison  -----
+flights.out <- cbind(flights[outward,],flights.characteristics[outward,],flights.weather[outward,],dif.angle[outward])
+
+flights.in <- cbind(flights[inward,],flights.characteristics[inward,],flights.weather[inward,],dif.angle[inward])
+
+
+# Re-order these by trip_id
+flights.out  <- flights.out[order(flights.out$trip_id),]
+flights.in   <- flights.in[order(flights.in$trip_id),]
+
+
+#Find and index those flights for which there is a corresponding outward or inward flight for same trip.
+x <- NA
+for( i in seq(along = flights.out$trip_id)){
+  if(any(flights.out$trip_id[i] == flights.in$trip_id)) x[i] = TRUE else{x[i] = FALSE}
+}
+
+y <- NA
+for( i in seq(along = flights.in$trip_id)){
+  if(any(flights.in$trip_id[i] == flights.out$trip_id)) y[i] = TRUE else{y[i] = FALSE}
+}
+
+
+# Check that this has worked
+all.equal(flights.in$trip_id[y] , flights.out$trip_id[x])
+
+flights.in <- flights.in[y,]
+flights.out <- flights.out[x,]
+
+length(flights.in$flight_id)
+
+length(unique(flights.in$device_info_serial))
+
+
+
+#Rearrange data for comparision
+flights.out$flight.type <- "out"
+flights.in$flight.type <- "in"
+
+flights.out$wind.type <- NA
+flights.in$wind.type <- NA
+
+flights.out$wind.type[flights.out$dif.angle < 60] <- "tail"
+flights.out$wind.type[flights.out$dif.angle > 60 & flights.out$dif.angle < 120] <- "side"
+flights.out$wind.type[flights.out$dif.angle > 120] <- "head"
+
+flights.in$wind.type[flights.in$dif.angle < 60] <- "tail"
+flights.in$wind.type[flights.in$dif.angle > 60 & flights.in$dif.angle < 120] <- "side"
+flights.in$wind.type[flights.in$dif.angle > 90] <- "head"
+
+
+
+names(flights.out)[length(flights.out)-2] <- "dif.angle"
+names(flights.in)[length(flights.in)-2] <- "dif.angle"
+# Check that names all correspond between the two tables.
+all.equal(names(flights.out), names(flights.in))
+
+
+# Combined table of outward and inward flights
+flights.combined <- rbind(flights.out,flights.in)
+
+# Order flights by date-time at start of flight
+flights.combined   <- flights.combined[order(flights.combined$start_time),]
+
+
+# Some function definitions --------
+
+# Logit transformation   ####
+logit <- function(x){
+  if(x <1.0001 & x > -0.0001){   #Return NAs for values outside of range 0 - 1, with small tollerance either way.
+    x.new <- 0.99999999999*x    #Bring all values in slightly, so that values of 1.0 can be processed.
+    #     x.new[x.new == 0] <- 0.0000000000001
+    x.new[x.new == 0] <- 0.001
+    x.logit <- log(x.new/(1-x.new))
+    return(x.logit)}
+  else return(NA)
+}
+
+
+anti.logit <- function(x){
+  ex <- exp(x)
+  fx <- ex/(ex + 1)
+  return(fx)
+}
+
+if.neg <- function(x){
+  if(x < 0) return(0)
+  else return(1)
+}
+
+
+
+
