e144850c7360ad3b2144e60b282e964aee017994
Tom Evans <thomas.jude.evans@gmail.com>
2014-02-02 22:36:30 +0100

    Quick but dirty stats analysis of altitude, Va, and straightness

--- commuting_flight_comparisons_categorised_stats.R
+++ commuting_flight_comparisons_categorised_stats.R
@@ -236,10 +236,11 @@ aggregate(speed_inst_mean ~ device_info_serial,
 
 # Altitude ------
 
-# Make a filter to this effect
+# Make a filter
 f <- flights.combined$alt_med < 150  &   flights.combined$alt_med > -20 & !is.na(flights.combined$wind_side_mean)
 # hist(flights.combined$alt_med)
 summary(f)
+
 
 # Retain only trips where both the outward and inward flight fulfill above conditions
 f2 <- flights.combined$trip_id %in% flights.combined$trip_id[!f]
@@ -331,30 +332,635 @@ mod[[1]]  <-  lme(alt_trans ~
                   random = ~1|device_info_serial/trip_id,
                   data = alt_data
                   )
-mod_ml[[1]] <- update()
+mod_ml[[1]] <- update(mod[[1]], method = "ML")
 summary(mod[[1]])
 
 
 mod[[2]]  <-  lme(alt_trans ~
                     (flight.type + temp_2m + cloud_total +
                        cloud_low_logit + head_tail_abs + side_abs +
-                       head_tail.type + distance +
-                       sea_level_pressure)^2,
+                       distance + sea_level_pressure +
+                       head_tail.type)^2,
                   random = ~1|device_info_serial/trip_id,
                   data = alt_data
 )
+mod_ml[[2]] <- update(mod[[2]], method = "ML")
+summary(mod[[2]])
 
-modb_ML <- update(modb, method = "ML")
+anova(mod_ML[[1]], mod_ML[[2]])
 
-anova(mod_ML[[1]], modb_ML)
+# Better to allow different slope for tail and head winds
 
-# Compare different random effect structure
-mod[[2]]  <-  update(mod[[1]], random = ~1|device_info_serial)
+mod[[3]] <-  update(mod[[2]], random = ~1|device_info_serial)
+anova(mod[[2]], mod[[3]])
 
-anova(mod[[1]], mod[[2]])
+# Better to include trip id in random effects - i.e taking account of nestedness
 
-# Spatio-temporal correlation
-plot(ACF(mod[[2]], maxLag = 10), alpha = 0.01)
-# No signs of strong autocorrelation, probably owing to model overfitting
+# Now simplify - only retain significant terms
 
-anova(mod[[2]])
+anova(mod_ML[[2]])
+
+mod[[4]]    <-  update(mod[[2]], .~. -distance:sea_level_pressure - cloud_low_logit:distance -  cloud_total:sea_level_pressure - temp_2m:sea_level_pressure)
+
+mod_ML[[4]]  <- update(mod[[4]], method = "ML")
+
+anova(mod[[4]])
+
+mod[[5]]    <-  update(mod[[4]], .~. -flight.type:distance -  temp_2m:cloud_total - temp_2m:cloud_low_logit - cloud_total:head_tail_abs - cloud_total:distance - cloud_low_logit:sea_level_pressure - head_tail_abs:distance  - head_tail_abs:sea_level_pressure - distance:head_tail.type)
+
+mod_ML[[5]] <- update(mod[[5]], method = "ML")
+
+anova(mod_ML[[4]], mod_ML[[5]])
+
+anova(mod[[5]])
+
+mod[[6]]    <-  update(mod[[5]], .~. -sea_level_pressure:head_tail.type - head_tail_abs:side_abs - side_abs:sea_level_pressure)
+
+mod_ML[[6]] <- update(mod[[6]], method = "ML")
+
+anova(mod_ML[[5]], mod_ML[[6]])
+
+anova(mod[[6]])
+
+
+
+mod[[7]]    <-  update(mod[[6]], .~. -temp_2m:distance - cloud_low_logit:head_tail_abs)
+
+mod_ML[[7]] <- update(mod[[7]], method = "ML")
+
+anova(mod_ML[[6]], mod_ML[[7]])
+
+anova(mod[[7]])
+
+mod[[8]]    <-  update(mod[[7]], .~. -cloud_low_logit:head_tail.type - flight.type:sea_level_pressure)
+
+mod_ML[[8]] <- update(mod[[8]], method = "ML")
+
+anova(mod_ML[[7]], mod_ML[[8]])
+
+anova(mod[[8]])
+
+
+mod[[9]]    <-  update(mod[[8]], .~. -flight.type:head_tail.type)
+
+mod_ML[[9]] <- update(mod[[9]], method = "ML")
+
+anova(mod_ML[[8]], mod_ML[[9]])
+
+anova(mod[[9]])
+
+
+mod[[10]]    <-  update(mod[[9]], .~. -side_abs:distance)
+
+mod_ML[[10]] <- update(mod[[10]], method = "ML")
+
+anova(mod_ML[[9]], mod_ML[[10]])
+
+anova(mod[[10]])
+
+
+
+mod[[11]]    <-  update(mod[[10]], .~. -cloud_total:cloud_low_logit)
+
+mod_ML[[11]] <- update(mod[[11]], method = "ML")
+
+anova(mod_ML[[10]], mod_ML[[11]])
+
+anova(mod[[11]])
+
+# Stick with mod[[11]] for now, not all significant, but mostly, and AIC now increases if terms are removed.
+
+# Look at model
+
+intervals(mod[[11]])
+
+# assumptions
+plot(mod[[11]], flight.type ~ resid(.), abline = 0)
+
+plot(mod[[11]], resid(., type = "p") ~ fitted(.) | device_info_serial, id = 0.05, adj = -0.3)
+
+plot(mod[[11]], alt_trans ~ fitted(.), id = 0.05, adj = -0.3)
+
+qqnorm(mod[[11]], ~resid(.)| flight.type)
+qqnorm(mod[[11]], ~resid(.)| device_info_serial)
+
+
+plot(mod[[11]])
+qqnorm(mod[[11]])
+
+
+library(MuMIn)
+r.squaredGLMM(mod[[11]])
+
+# Explains 48% of variation in altitude, with 39% down to main effects, other 9% random effects
+
+
+
+
+# Va -------
+
+f <-  !is.na(flights.combined$wind_side_mean)
+summary(f)
+# names(flights.combined)
+# hist(flights.combined$head_speed_mean)
+
+f2 <- flights.combined$trip_id %in% flights.combined$trip_id[!f]
+f3 <- !f2
+summary(f3)
+
+
+
+# Prepare variables
+device_info_serial <- flights.combined$device_info_serial[f3]
+device_info_serial[device_info_serial == 687] <- 596
+device_info_serial <- as.factor(device_info_serial)
+summary(device_info_serial)
+
+trip_id <- trip <- as.factor(flights.combined$trip_id[f3])
+
+flight.type <- as.factor(flights.combined$flight.type[f3])
+
+date.time <- flights.combined$start_time[f3]
+
+# Temperature at 2m level
+temp_2m <- flights.combined$temperature_2mmean[f3]  -273.15
+# hist(temp_2m)
+
+# Total atomospheric cloud cover
+cloud_total <- flights.combined$cloud_cover_totalmean[f3]
+hist(cloud_total)
+hist(logit(cloud_total))
+cloud_total_logit <- logit(cloud_total)
+
+# Low level cloud (may reflect atmospheric visibility better)
+cloud_low <- flights.combined$cloud_cover_low_altitudemean[f3]
+hist(cloud_low)
+cloud_low_logit <- sapply(cloud_low,logit)
+hist(cloud_low_logit)
+
+# Wind data
+side.type <- as.factor(sapply(flights.combined$wind_side_mean_10[f3], if.neg))
+head_tail.type <- as.factor(sapply(flights.combined$wind_head_tail_mean_10[f3], if.neg))
+head_tail_abs <- abs(flights.combined$wind_head_tail_mean_10[f3])
+side_abs <- abs(flights.combined$wind_side_mean_10[f3])
+
+head_tail_vec <- (flights.combined$wind_head_tail_mean_10[f3])
+# hist(head_tail_vec)
+side_vec <- (flights.combined$wind_side_mean_10[f3])
+# hist(side_vec)
+
+# Flight distance
+distance <- flights.combined$dist_a_b[f3]
+hist(distance/1000)  # distances in km
+
+sea_level_pressure <- flights.combined$sea_level_pressuremean[f3]
+hist(sea_level_pressure)
+
+# Va
+va <-  flights.combined$head_speed_mean[f3]
+
+# Compile variables into single dataframe
+va_data <- cbind(device_info_serial, trip_id, flight.type,
+                  date.time, temp_2m,va , cloud_total,
+                  cloud_total_logit, cloud_low,
+                  cloud_low_logit, side_abs,
+                   side.type, head_tail.type,
+                  head_tail_abs, head_tail_vec, side_vec,
+                  distance, sea_level_pressure)
+va_data <- as.data.frame(va_data)
+
+str(va_data)
+va_data$device_info_serial <- as.factor(va_data$device_info_serial)
+va_data$trip_id <- as.factor(va_data$trip_id)
+va_data$flight.type  <- as.factor(va_data$flight.type)
+va_data$side.type   <- as.factor(va_data$side.type)
+va_data$head_tail.type   <- as.factor(va_data$head_tail.type)
+
+
+# Statistics
+library(nlme)
+
+mod <- list()
+mod_ml <- list()
+
+# full model with all 2-way interactions included
+mod[[1]]  <-  lme(va ~
+                    (flight.type + temp_2m + cloud_total +
+                       cloud_low_logit + head_tail_vec + side_abs +
+                       distance + sea_level_pressure)^2,
+                  random = ~1|device_info_serial/trip_id,
+                  data = va_data
+)
+mod_ml[[1]] <- update(mod[[1]], method = "ML")
+summary(mod[[1]])
+
+
+mod[[2]]  <-  lme(va ~
+                    (flight.type + temp_2m + cloud_total +
+                       cloud_low_logit + head_tail_abs + side_abs +
+                       distance + sea_level_pressure +
+                       head_tail.type)^2,
+                  random = ~1|device_info_serial/trip_id,
+                  data = va_data
+)
+mod_ml[[2]] <- update(mod[[2]], method = "ML")
+summary(mod[[2]])
+
+anova(mod_ML[[1]], mod_ML[[2]])
+
+# Better to keep Va as vector, not sepperate head/ tail
+
+mod[[3]] <-  update(mod[[1]], random = ~1|device_info_serial)
+anova(mod[[1]], mod[[3]])
+
+# Better not to include trip id in random effects
+
+# May be worth looking at correlation structure later
+plot(ACF(mod[[3]], maxLag = 10), alpha = 0.01)
+
+# Now simplify - only retain significant terms
+
+anova(mod_ML[[3]])
+
+mod[[4]]    <-  update(mod[[3]], .~. -cloud_low_logit:distance - cloud_total:sea_level_pressure - temp_2m:sea_level_pressure - temp_2m:cloud_total)
+
+mod_ML[[4]]  <- update(mod[[4]], method = "ML")
+
+anova(mod[[4]])
+
+mod[[5]]    <-  update(mod[[4]], .~. -distance:sea_level_pressure - head_tail_vec:sea_level_pressure - cloud_low_logit:head_tail_vec - temp_2m:distance - temp_2m:side_abs)
+
+mod_ML[[5]]  <- update(mod[[5]], method = "ML")
+
+anova(mod_ML[[5]], mod_ML[[4]])
+anova(mod[[5]])
+
+
+
+mod[[6]]    <-  update(mod[[5]], .~. -flight.type:temp_2m - flight.type:cloud_low_logit - flight.type:distance)
+
+mod_ML[[6]]  <- update(mod[[6]], method = "ML")
+
+anova(mod_ML[[6]], mod_ML[[5]])
+anova(mod[[6]])
+
+
+mod[[7]]    <-  update(mod[[6]], .~. -temp_2m:cloud_low_logit - cloud_total:cloud_low_logit)
+
+mod_ML[[7]]  <- update(mod[[7]], method = "ML")
+
+anova(mod_ML[[7]], mod_ML[[6]])
+anova(mod[[7]])
+
+
+mod[[8]]    <-  update(mod[[7]], .~. -flight.type:sea_level_pressure - side_abs:sea_level_pressure)
+
+mod_ML[[8]]  <- update(mod[[8]], method = "ML")
+
+anova(mod_ML[[8]], mod_ML[[7]])
+anova(mod[[8]])
+
+
+mod[[9]]    <-  update(mod[[8]], .~. -cloud_low_logit:side_abs)
+
+mod_ML[[9]]  <- update(mod[[9]], method = "ML")
+
+anova(mod_ML[[9]], mod_ML[[8]])
+anova(mod[[9]])
+
+
+mod[[10]]    <-  update(mod[[9]], .~. -cloud_total:side_abs)
+
+mod_ML[[10]]  <- update(mod[[10]], method = "ML")
+
+anova(mod_ML[[10]], mod_ML[[9]])
+anova(mod[[10]])
+
+
+mod[[11]]    <-  update(mod[[10]], .~. -cloud_low_logit:sea_level_pressure - cloud_total:distance - cloud_total:head_tail_vec - flight.type:cloud_total)
+
+mod_ML[[11]]  <- update(mod[[11]], method = "ML")
+
+anova(mod_ML[[11]], mod_ML[[10]])
+anova(mod[[11]])
+
+
+mod[[12]]    <-  update(mod[[11]], .~. -cloud_low_logit - cloud_total)
+
+mod_ML[[12]]  <- update(mod[[12]], method = "ML")
+
+anova(mod_ML[[12]], mod_ML[[11]])
+anova(mod[[12]])
+
+anova(mod_ML[[9]], mod_ML[[12]])
+
+# Stick with model 12, all significant
+
+
+
+# Look at model
+
+intervals(mod[[12]])
+anova(mod[[12]])
+VarCorr(mod[[12]])
+summary(mod[[12]])
+
+# assumptions
+plot(mod[[12]], flight.type ~ resid(.), abline = 0)
+
+plot(mod[[12]], resid(., type = "p") ~ fitted(.) | device_info_serial, id = 0.05, adj = -0.3)
+
+plot(mod[[12]], va ~ fitted(.), id = 0.05, adj = -0.3)
+
+qqnorm(mod[[12]], ~resid(.)| flight.type)
+qqnorm(mod[[12]], ~resid(.)| device_info_serial)
+
+
+plot(mod[[12]])
+qqnorm(mod[[12]])
+
+
+library(MuMIn)
+r.squaredGLMM(mod[[12]])
+
+# Explains 34% of variation in altitude, with 30% down to main effects, other 3% random effects
+
+
+
+# Straightness -----
+hist(flights.combined$straigtness)
+hist(logit(flights.combined$straigtness))
+
+
+f <-  !is.na(flights.combined$wind_side_mean)
+summary(f)
+# names(flights.combined)
+# hist(flights.combined$head_speed_mean)
+
+f2 <- flights.combined$trip_id %in% flights.combined$trip_id[!f]
+f3 <- !f2
+summary(f3)
+
+
+
+# Prepare variables
+device_info_serial <- flights.combined$device_info_serial[f3]
+device_info_serial[device_info_serial == 687] <- 596
+device_info_serial <- as.factor(device_info_serial)
+summary(device_info_serial)
+
+trip_id <- trip <- as.factor(flights.combined$trip_id[f3])
+
+flight.type <- as.factor(flights.combined$flight.type[f3])
+
+date.time <- flights.combined$start_time[f3]
+
+# Temperature at 2m level
+temp_2m <- flights.combined$temperature_2mmean[f3]  -273.15
+# hist(temp_2m)
+
+# Total atomospheric cloud cover
+cloud_total <- flights.combined$cloud_cover_totalmean[f3]
+hist(cloud_total)
+hist(logit(cloud_total))
+cloud_total_logit <- logit(cloud_total)
+
+# Low level cloud (may reflect atmospheric visibility better)
+cloud_low <- flights.combined$cloud_cover_low_altitudemean[f3]
+hist(cloud_low)
+cloud_low_logit <- sapply(cloud_low,logit)
+hist(cloud_low_logit)
+
+# Wind data
+side.type <- as.factor(sapply(flights.combined$wind_side_mean_10[f3], if.neg))
+head_tail.type <- as.factor(sapply(flights.combined$wind_head_tail_mean_10[f3], if.neg))
+head_tail_abs <- abs(flights.combined$wind_head_tail_mean_10[f3])
+side_abs <- abs(flights.combined$wind_side_mean_10[f3])
+
+head_tail_vec <- (flights.combined$wind_head_tail_mean_10[f3])
+# hist(head_tail_vec)
+side_vec <- (flights.combined$wind_side_mean_10[f3])
+# hist(side_vec)
+
+# Flight distance
+distance <- flights.combined$dist_a_b[f3]
+hist(distance/1000)  # distances in km
+
+sea_level_pressure <- flights.combined$sea_level_pressuremean[f3]
+hist(sea_level_pressure)
+
+
+
+
+straight <- (flights.combined$straigtness[f3])
+straight_logit <-  (logit(flights.combined$straigtness[f3]))
+
+
+
+# Compile variables into single dataframe
+s_data <- cbind(device_info_serial, trip_id, flight.type,
+                 date.time, temp_2m,
+                 straight, straight_logit, cloud_total,
+                 cloud_total_logit, cloud_low,
+                 cloud_low_logit, side_abs,
+                 side.type, head_tail.type,
+                 head_tail_abs, head_tail_vec, side_vec,
+                 distance, sea_level_pressure)
+s_data <- as.data.frame(s_data)
+
+str(s_data)
+s_data$device_info_serial <- as.factor(s_data$device_info_serial)
+s_data$trip_id <- as.factor(s_data$trip_id)
+s_data$flight.type  <- as.factor(s_data$flight.type)
+s_data$side.type   <- as.factor(s_data$side.type)
+s_data$head_tail.type   <- as.factor(s_data$head_tail.type)
+
+
+
+# Statistics
+library(nlme)
+
+mod <- list()
+mod_ml <- list()
+
+# full model with all 2-way interactions included
+mod[[1]]  <-  lme(straight_logit ~
+                    (flight.type + temp_2m + cloud_total +
+                       cloud_low_logit + head_tail_vec + side_abs +
+                       distance + sea_level_pressure)^2,
+                  random = ~1|device_info_serial/trip_id,
+                  data = s_data
+)
+mod_ml[[1]] <- update(mod[[1]], method = "ML")
+summary(mod[[1]])
+
+
+mod[[2]]  <-  lme(straight_logit ~
+                    (flight.type + temp_2m + cloud_total +
+                       cloud_low_logit + head_tail_abs + side_abs +
+                       distance + sea_level_pressure +
+                       head_tail.type)^2,
+                  random = ~1|device_info_serial/trip_id,
+                  data = s_data
+)
+mod_ml[[2]] <- update(mod[[2]], method = "ML")
+summary(mod[[2]])
+
+anova(mod_ML[[1]], mod_ML[[2]])
+
+# Better to keep Va as vector, not sepperate head/ tail
+
+mod[[3]] <-  update(mod[[1]], random = ~1|device_info_serial)
+anova(mod[[1]], mod[[3]])
+
+# Better not to include trip id in random effects
+
+# May be worth looking at correlation structure later
+plot(ACF(mod[[3]], maxLag = 10), alpha = 0.01)
+
+
+# Now simplify - only retain significant terms
+
+anova(mod_ML[[3]])
+
+mod[[4]]    <-  update(mod[[3]], .~. -cloud_total:sea_level_pressure - cloud_low_logit:distance - temp_2m:sea_level_pressure)
+
+mod_ML[[4]]  <- update(mod[[4]], method = "ML")
+
+anova(mod[[4]])
+
+
+mod[[5]]    <-  update(mod[[4]], .~. -side_abs:sea_level_pressure - head_tail_vec:distance - cloud_total:distance - cloud_total:head_tail_vec - temp_2m:cloud_total)
+
+mod_ML[[5]]  <- update(mod[[5]], method = "ML")
+
+anova(mod[[5]])
+
+
+mod[[6]]    <-  update(mod[[5]], .~. -cloud_total:cloud_low_logit - temp_2m:head_tail_vec - flight.type:distance - flight.type:cloud_low_logit)
+
+mod_ML[[6]]  <- update(mod[[6]], method = "ML")
+
+anova(mod_ML[[5]], mod_ML[[6]])
+
+anova(mod[[6]])
+
+
+mod[[7]]    <-  update(mod[[6]], .~. -head_tail_vec:sea_level_pressure - cloud_total:side_abs - flight.type:side_abs - flight.type:temp_2m)
+
+mod_ML[[7]]  <- update(mod[[7]], method = "ML")
+
+anova(mod_ML[[7]], mod_ML[[6]])
+
+anova(mod[[7]])
+
+
+
+mod[[8]]    <-  update(mod[[7]], .~. -temp_2m:side_abs)
+
+mod_ML[[8]]  <- update(mod[[8]], method = "ML")
+
+anova(mod_ML[[8]], mod_ML[[7]])
+
+anova(mod[[8]])
+
+mod[[9]]    <-  update(mod[[8]], .~. -cloud_low_logit:head_tail_vec)
+
+mod_ML[[9]]  <- update(mod[[9]], method = "ML")
+
+anova(mod_ML[[9]], mod_ML[[8]])
+
+anova(mod[[9]])
+
+
+mod[[10]]    <-  update(mod[[9]], .~. -distance:sea_level_pressure)
+
+mod_ML[[10]]  <- update(mod[[10]], method = "ML")
+
+anova(mod_ML[[10]], mod_ML[[9]])
+
+anova(mod[[10]])
+
+
+mod[[11]]    <-  update(mod[[10]], .~. -temp_2m:distance - temp_2m:cloud_low_logit - temp_2m)
+
+mod_ML[[11]]  <- update(mod[[11]], method = "ML")
+
+anova(mod_ML[[11]], mod_ML[[9]])
+
+anova(mod[[11]])
+
+
+mod[[12]]    <-  update(mod[[11]], .~. -cloud_low_logit:sea_level_pressure)
+
+mod_ML[[12]]  <- update(mod[[12]], method = "ML")
+
+anova(mod_ML[[12]], mod_ML[[11]])
+
+anova(mod[[12]])
+
+
+mod[[13]]    <-  update(mod[[12]], .~. -cloud_total - flight.type:cloud_total)
+
+mod_ML[[13]]  <- update(mod[[13]], method = "ML")
+
+anova(mod_ML[[13]], mod_ML[[11]])
+
+anova(mod[[13]])
+
+
+
+mod[[14]]    <-  update(mod[[13]], .~. -flight.type:head_tail_vec)
+
+mod_ML[[14]]  <- update(mod[[14]], method = "ML")
+
+anova(mod_ML[[14]], mod_ML[[11]])
+
+anova(mod[[14]])
+
+
+mod[[15]]    <-  update(mod[[14]], .~. -flight.type:sea_level_pressure - sea_level_pressure)
+
+mod_ML[[15]]  <- update(mod[[15]], method = "ML")
+
+anova(mod_ML[[15]], mod_ML[[11]])
+
+anova(mod[[15]])
+
+
+mod[[16]]    <-  update(mod[[15]], .~. -head_tail_vec:side_abs)
+
+mod_ML[[16]]  <- update(mod[[16]], method = "ML")
+
+anova(mod_ML[[16]], mod_ML[[11]])
+
+anova(mod[[16]])
+
+
+
+# Look at model
+
+intervals(mod[[16]])
+anova(mod[[16]])
+VarCorr(mod[[16]])
+summary(mod[[16]])
+
+# assumptions
+plot(mod[[16]], flight.type ~ resid(.), abline = 0)
+
+plot(mod[[16]], resid(., type = "p") ~ fitted(.) | device_info_serial, id = 0.05, adj = -0.3)
+
+plot(mod[[16]], straight_logit ~ fitted(.), id = 0.05, adj = -0.3)
+
+qqnorm(mod[[16]], ~resid(.)| flight.type)
+qqnorm(mod[[16]], ~resid(.)| device_info_serial)
+
+
+plot(mod[[16]])
+qqnorm(mod[[16]])
+
+
+library(MuMIn)
+r.squaredGLMM(mod[[16]])
+
+# Explains 18% of variation in altitude, with 17% down to main effects, other 1% random effects
