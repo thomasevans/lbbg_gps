63cd58988f0fa6f1945f18dd17c659fdc92bd877
Tom Evans <thomas.jude.evans@gmail.com>
2014-02-02 12:22:58 +0100

    Compiling dataframe of data for altitude analysis

--- commuting_flight_comparisons_categorised_stats.R
+++ commuting_flight_comparisons_categorised_stats.R
@@ -253,4 +253,59 @@ device_info_serial[device_info_serial == 687] <- 596
 device_info_serial <- as.factor(device_info_serial)
 summary(device_info_serial)
 
+trip_id <- trip <- as.factor(flights.combined$trip_id[f3])
+
+flight.type <- as.factor(flights.combined$flight.type[f3])
+
+date.time <- flights.combined$start_time[f3]
+
+# Temperature at 2m level
+temp_2m <- flights.combined$temperature_2mmean[f3]  -273.15
+# hist(temp_2m)
+
+# Total atomospheric cloud cover
+cloud_total <- flights.combined$cloud_cover_totalmean[f3]
+hist(cloud_total)
+hist(logit(cloud_total))
+cloud_total_logit <- logit(cloud_total)
+
+# Low level cloud (may reflect atmospheric visibility better)
+cloud_low <- flights.combined$cloud_cover_low_altitudemean[f3]
+hist(cloud_low)
+cloud_low_logit <- sapply(cloud_low,logit)
+hist(cloud_low_logit)
+
+# Altitude responce variable - including transformation
+altitude <- flights.combined$alt_med[f3]
+hist(altitude)
+alt_trans <- log10(flights.combined$alt_med[f3]+20)
+hist(alt_trans)
+
+# Wind data
+side.type <- as.factor(sapply(flights.combined$wind_side_mean_10[f3], if.neg))
+head_tail.type <- as.factor(sapply(flights.combined$wind_head_tail_mean_10[f3], if.neg))
+head_tail_abs <- abs(flights.combined$wind_head_tail_mean_10[f3])
+side_abs <- abs(flights.combined$wind_side_mean_10[f3])
+
+head_tail_vec <- (flights.combined$wind_head_tail_mean_10[f3])
+# hist(head_tail_vec)
+side_vec <- (flights.combined$wind_side_mean_10[f3])
+# hist(side_vec)
+
+# Flight distance
+distance <- flights.combined$dist_a_b[f3]
+hist(distance/1000)  # distances in km
+
+sea_level_pressure <- flights.combined$sea_level_pressuremean[f3]
+hist(sea_level_pressure)
+
+# Compile variables into single dataframe
+alt_data <- cbind(device_info_serial, trip_id, flight.type,
+                  date.time, temp_2m, cloud_total,
+                  cloud_total_logit, cloud_low,
+                  cloud_low_logit, altitude,
+                  alt_trans, side.type, head_tail.type,
+                  head_tail_abs, head_tail_vec, side_vec,
+                  distance, sea_level_pressure)
+alt_data <- as.data.frame(alt_data)
 
