751cccb3c30e9549ea069f59ba227c2c8e3fa811
Tom Evans <thomas.jude.evans@gmail.com>
2014-02-07 08:56:49 +0100

    New script for analysis of inward flights with respect to wind - drift

---
+++ commuting_flight_inward_drift.R
@@ -0,0 +1,9 @@
+# Developed by Tom Evans at Lund University: tom.evans@biol.lu.se
+# You are welcome to use parts of this code, but please give credit when using it extensively.
+# Code available at https://github.com/thomasevans/lbbg_gps
+
+# Description -----
+# This script goes through all inward flights calculating for each flight point various paramaters associated with wind drift. Once calculated these are then output to a new database table.
+
+
+# Database data downloand ----
