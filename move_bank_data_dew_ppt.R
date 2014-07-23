cd1b4db3809148a304dceefbf14a20fa5a20a713
Tom Evans <thomas.jude.evans@gmail.com>
2014-07-23 19:45:26 +0200

    New script to parse Move-bank extracted data for dew point and preciptitation, then output to a new database table. Final version

---
+++ move_bank_data_dew_ppt.R
@@ -0,0 +1,58 @@
+#Primarily developed by Tom Evans at Lund University: tom.evans@biol.lu.se
+#You are welcome to use parts of this code, but please give credit when using it extensively.
+
+#' Data from MoveBank for two vairables
+#' Here we parse the CSV file to output to a new
+#' DB table with variables
+#' - date_time
+#' - device_info_serial
+#' - ecwf_dew_point
+#' - ecwf_ppt
+
+
+
+setwd("D:/Dropbox/LBBG_Movebank/Data_downloaded/MoveBank_data_all")                            
+
+n <- count.fields("ECMWF_ppt_dew_point.csv", sep = ",")
+head(n)
+col.num <- max(n)
+
+
+mycols <- rep("NULL", col.num)
+mycols[c(3, 9, col.num -1, col.num)] <- NA
+data.com <- read.table("ECMWF_ppt_dew_point.csv", sep = ",", colClasses = mycols, skip = 1, row.names = NULL)
+# ?read.table
+names(data.com) <- c("date_time", "device_info_serial",
+                     "ecwf_ppt", "ecwf_dew_point")
+
+head(data.com)
+
+
+# Get datetime to correct format
+data.com$datetime <- as.POSIXct(as.character(data.com$datetime), tz = "UTC")
+
+str(data.com)
+
+as.numeric.factor <- function(x) {as.numeric(levels(x))[x]}
+
+
+data.com$ecwf_ppt <- as.numeric.factor(data.com$ecwf_ppt)
+data.com$ecwf_dew_point <- as.numeric.factor(data.com$ecwf_dew_point)
+
+str(data.com)
+# hist(data.com$ecwf_dew_point)
+# hist(data.com$ecwf_ppt)
+
+# connection to DB
+library(RODBC)
+
+#Establish a connection to the database
+gps.db <- odbcConnectAccess2007('D:/Documents/Work/GPS_DB/GPS_db.accdb')
+
+
+# Output to new table in the database. ----
+sqlSave(gps.db, data.com, tablename = "move_bank_ppt_dew_ECMWF",
+        append = FALSE, rownames = FALSE, colnames = FALSE,
+        verbose = FALSE, safer = TRUE, addPK = FALSE, fast = TRUE,
+        test = FALSE, nastring = NULL,
+        varTypes =  c(date_time = "datetime"))
