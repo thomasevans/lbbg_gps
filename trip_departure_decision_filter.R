850e9a3b07e9733832fbb2fbaea38c277cdac4ce
Tom Evans <thomas.jude.evans@gmail.com>
2014-07-26 15:07:54 +0200

    New script to filter the data on trip departure descisions.

---
+++ trip_departure_decision_filter.R
@@ -0,0 +1,11 @@
+# Primarily developed by Tom Evans at Lund University: tom.evans@biol.lu.se
+# You are welcome to use parts of this code, but please give credit when using it extensively.
+
+# This is script is to filter the trip_departure_decision
+# data to retain only relevant trips. For example it will
+# only include trips of a minimum time duration and distance
+# plus will exclude trips of too great a time duration.
+# Further it filters by periods, so we only retain data
+# for relevant months to our analysis (May, June, July),
+# months during the core breeding season.
+
