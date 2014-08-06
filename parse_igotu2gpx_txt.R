4677fa5b5cbfc2207635f2a3de2410377e5e6eb3
Tom Evans <thomas.jude.evans@gmail.com>
2014-08-06 09:47:03 +0200

    Script to parse the RAW text data files from igotu2gpx

---
+++ parse_igotu2gpx_txt.R
@@ -0,0 +1,6 @@
+# Primarily developed by Tom Evans at Lund University: tom.evans@biol.lu.se
+# You are welcome to use parts of this code, but please give credit when using it extensively.
+
+# This script is to extract data from RAW text files produced
+# by igotu2gpx. Parsing the text and then outputint to file.
+
