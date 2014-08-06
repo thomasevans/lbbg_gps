b0747e852ed2b44e4e852a4c39fdb5ad39a6c1e0
Tom Evans <thomas.jude.evans@gmail.com>
2014-08-06 13:10:37 +0200

    Get further variables (latitude, longitude, elevation, speed, course, ehpe, timeout, MSVs_QCN)

--- parse_igotu2gpx_txt.R
+++ parse_igotu2gpx_txt.R
@@ -46,5 +46,95 @@ date_time <- as.POSIXct(date.time.only, tz = "UTC")
 # str(x)
 
 
+# Get latitude ----
+# Get index for line in sub.text with latitude
+i.lat <- grep("Latitude", sub.text)
+# Just lat line
+lat.txt <- sub.text[i.lat]
+# lat only
+lat.only <- strsplit(lat.txt, split = "  Latitude " )[[1]][2]
+lat <- as.numeric(lat.only)
+
+
+# Get longitude ----
+# Get index for line in sub.text with longitude
+i.long <- grep("Longitude", sub.text)
+# Just long line
+long.txt <- sub.text[i.long]
+# long only
+long.only <- strsplit(long.txt, split = "  Longitude " )[[1]][2]
+long <- as.numeric(long.only)
+
+
+# Get elevation ----
+# Get index for line in sub.text with elevation
+i.elev <- grep("Elevation", sub.text)
+# Just elev line
+elev.txt <- sub.text[i.elev]
+# elev only
+elev.only <- strsplit(elev.txt, split = "  Elevation " )[[1]][2]
+elev.only <- strsplit(elev.only, split = " m" )[[1]][1]
+elev <- as.numeric(elev.only)
+
+
+
+# Get speed ----
+# Get index for line in sub.text with speedation
+i.speed <- grep("Speed", sub.text)
+# Just speed line
+speed.txt <- sub.text[i.speed]
+# speed only
+speed.only <- strsplit(speed.txt, split = "  Speed " )[[1]][2]
+speed.only <- strsplit(speed.only, split = " km/h" )[[1]][1]
+speed <- as.numeric(speed.only)
+
+
+# Get course ----
+# Get index for line in sub.text with course
+i.course <- grep("Course", sub.text)
+# Just course line
+course.txt <- sub.text[i.course]
+# course only
+course.only <- strsplit(course.txt, split = "  Course " )[[1]][2]
+course.only <- strsplit(course.only, split = " degrees" )[[1]][1]
+course <- as.numeric(course.only)
+
+
+# Get ehpe ----
+# Get index for line in sub.text with ehpe
+i.ehpe <- grep("EHPE", sub.text)
+# Just ehpe line
+ehpe.txt <- sub.text[i.ehpe]
+# ehpe only
+ehpe.only <- strsplit(ehpe.txt, split = "  EHPE " )[[1]][2]
+ehpe.only <- strsplit(ehpe.only, split = " m" )[[1]][1]
+ehpe <- as.numeric(ehpe.only)
+
+
+# Get Timeout -----
+# Possibly can get times of diving by looking at this, if timeout is
+# 12s with no fix appears to probably be diving.
+# Get index for line in sub.text with timeout
+i.timeout <- grep("Timeout", sub.text)
+# Just timeout line
+timeout.txt <- sub.text[i.timeout]
+# timeout only
+timeout.only <- strsplit(timeout.txt, split = "  Timeout " )[[1]][2]
+timeout.only <- strsplit(timeout.only, split = " s" )[[1]][1]
+timeout <- as.numeric(timeout.only)
+
+
+# Get MSVs_QCN ----
+# Get index for line in sub.text with ehpe
+i.MSVs_QCN <- grep("MSVs_QCN", sub.text)
+# Just MSVs_QCN line
+MSVs_QCN.txt <- sub.text[i.MSVs_QCN]
+# MSVs_QCN only
+MSVs_QCN.only <- strsplit(MSVs_QCN.txt, split = "  MSVs_QCN " )[[1]][2]
+MSVs_QCN <- as.numeric(MSVs_QCN.only)
+
+
+# Satellites:
+
 
 
