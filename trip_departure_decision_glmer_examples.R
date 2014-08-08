bb169dc84999e7c98397c3a3ecd32b5bd86ab9d4
Tom Evans <thomas.jude.evans@gmail.com>
2014-08-08 08:31:35 +0200

    Added R-square calculation

--- trip_departure_decision_glmer_examples.R
+++ trip_departure_decision_glmer_examples.R
@@ -43,6 +43,38 @@ mod.pressure.uplift.new <-glmer(
 summary(mod.pressure.uplift)
 
 
+# Get R-square -----
+# First install required library/ package
+install.packages("MuMIn")
+
+# Load package
+library("MuMIn")
+
+# R-square
+r.squaredGLMM(mod.pressure.uplift.new)
+# It gives two values
+# R2m and R2c
+# One is the 'marginal R-square', the other the
+# 'conditional R-square'
+# See explanation here
+?r.squaredGLMM
+# and associated paper here:
+browseURL("http://onlinelibrary.wiley.com/doi/10.1111/j.2041-210x.2012.00261.x/abstract")
+
+r.squaredGLMM(mod.pressure.uplift.new)
+# In this example the 'conditional R-square' (R2c)
+# is much greater than the 'model R-square' (R2m)
+# this suggests that individual birds are quite
+# different, with individual explaining much of
+# the variation in whether to forage on Gotland
+# or not. The main-effects (here sea-level pressure
+# and thermal-uplift) then explain only a small
+# proportion of variance.
+# Oh yeah - BTW, values are between 0 - 1, giving
+# proportion of overal variance explained. If you
+# multiply by 100 - you can view it has a %
+
+
 
 # Run mod.month.year ------
 mod.month.year <- glmer(gotland_fac ~ month * year +
