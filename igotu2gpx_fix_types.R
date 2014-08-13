721bda4cd5e8a8f6c196270f5afa3e3eddaa746f
Tom Evans <thomas.jude.evans@gmail.com>
2014-08-13 15:19:10 +0200

    Pseudo code of what to do in script

--- igotu2gpx_fix_types.R
+++ igotu2gpx_fix_types.R
@@ -5,3 +5,28 @@
 # annotate the GPS fix data from the IGU loggers
 # labelling points by whether they are vallid
 # GPS fixes, plus weather they include diving etc.
+
+# Read in DB data -----
+
+
+# Label by whether vallid GPS fix -----
+
+
+# Label by 'behaviour' ------
+#* 1. Flight
+#* 2. Diving (apparent)
+#* 3. Colony
+#* 4. Water surace (swimming etc)
+#* 5. Other (i.e. uncategorised)
+
+
+# Some labelling of previous/ next points? -----
+# Could be good for example to label if prio point was diving
+# so as to plot points according to this.
+# When plotting could get average/ intermediate position for diving
+# events
+
+# Output to new DB table ----
+# Output annotation to new DB table include device_info_serial
+# and date_time for primary key data
+
