1afd392f5b5490ee07e9eff0b8a43408a6157b03
Tom Evans <thomas.jude.evans@gmail.com>
2014-08-13 15:05:28 +0200

    New script to annotate guillemot IGU GPS point data

---
+++ igotu2gpx_fix_types.R
@@ -0,0 +1,7 @@
+# Primarily developed by Tom Evans at Lund University: tom.evans@biol.lu.se
+# You are welcome to use parts of this code, but please give credit when using it extensively.
+
+# This script is to produce a new database table to
+# annotate the GPS fix data from the IGU loggers
+# labelling points by whether they are vallid
+# GPS fixes, plus weather they include diving etc.
