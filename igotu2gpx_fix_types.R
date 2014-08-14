af877fadd4132c6e4a5f29a90428717711639f8f
Tom Evans <thomas.jude.evans@gmail.com>
2014-08-14 11:57:09 +0200

    DB querry, valid GPS points, good GPS points

--- igotu2gpx_fix_types.R
+++ igotu2gpx_fix_types.R
@@ -7,16 +7,51 @@
 # GPS fixes, plus weather they include diving etc.
 
 # Read in DB data -----
+# DB package
+library("RODBC")
+
+# Connect to DB
+gps.db <- odbcConnectAccess2007('D:/Documents/Work/GPS_DB/GPS_db.accdb')
+
+# Get GPS data
+points <- sqlQuery(gps.db,
+  query = "SELECT DISTINCT f.*
+          FROM guillemots_gps_points_igu AS f
+          ORDER BY f.device_info_serial ASC, f.date_time;",
+  as.is = TRUE)
 
 
 # Label by whether vallid GPS fix -----
+
+# Has location?
+gps_val <- (points$long != 0) & (points$lat != 0)
+summary(gps_val)
+
+# Is good quality GPS fix?
+f0 <- gps_val & (points$timeout <= 100)
+f1 <- gps_val & (points$ehpe <= 100)
+# f2 <- gps_val & (points$timeout <= 150)
+gps_ok <- f0 & f1
+summary(gps_ok)
+# gps_ok2 <- f0 & f2
+# summary(gps_ok2)
+
+# 
+# map.trip(points = points)
+# points(points$lat[!gps_ok]~points$long[!gps_ok],
+#        col = "magenta", pch = 8, cex = 1.2)
+# points(points$lat[!gps_ok2]~points$long[!gps_ok2],
+#        col = "green", pch = 8, cex = 1.2)
+
+
+
 
 
 # Label by 'behaviour' ------
 #* 1. Flight
 #* 2. Diving (apparent)
 #* 3. Colony
-#* 4. Water surace (swimming etc)
+#* 4. Water surface (swimming etc)
 #* 5. Other (i.e. uncategorised)
 
 
