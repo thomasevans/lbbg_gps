231f0ee154b2b51dd6540e668804d861de52495a
Tom Evans <thomas.jude.evans@gmail.com>
2014-08-24 23:52:30 +0200

    DB queries to GPS point data for both device types

--- guillemot_trip_classification.R
+++ guillemot_trip_classification.R
@@ -14,6 +14,54 @@
 # period.
 
 # Will need to do sepprately for the IGU and UvA devices
+# Get IGU GPS data
+
+# Read in DB data -----
+# DB package
+library("RODBC")
+
+# Connect to DB
+gps.db <- odbcConnectAccess2007('D:/Dropbox/tracking_db/GPS_db.accdb')
+
+
+# Get deployments table
+deployments <- sqlQuery(gps.db,
+                   query = "SELECT DISTINCT d.*
+          FROM guillemots_track_session AS d
+          ORDER BY d.device_info_serial ASC;",
+                   as.is = TRUE)
+
+
+
+# Get UvA GPS data
+# Get data for all devices attached to guillemots
+# First get a list of device numbers from deployments table.
+uva_dep_dev <- deployments$device_info_serial[
+  deployments$device_type == "uva"]
+
+igu_dep_dev <- deployments$device_info_serial[
+  deployments$device_type == "igu"]
+
+points_uva <- sqlQuery(gps.db,
+                        query = paste("SELECT DISTINCT g.device_info_serial, g.date_time, g.latitude, g.longitude
+          FROM gps_ee_tracking_speed_limited AS g
+          WHERE g.device_info_serial IN (", paste(unique(uva_dep_dev), collapse = ", "),
+          ") 
+          ORDER BY g.device_info_serial ASC, g.date_time ASC ;", sep = ""),
+                        as.is = TRUE)
+
+
+# Get IGU data
+points_igu <- sqlQuery(gps.db,
+                       query = "SELECT DISTINCT g.device_info_serial, g.date_time, g.latitude, g.longitude
+          FROM guillemots_gps_points_igu AS g 
+          ORDER BY g.device_info_serial ASC, g.date_time ASC ;",
+                       as.is = TRUE)
+
+
+# Filter data to only include data when devices are deployed.
+
+
 
 
 # Make some calculations on GPS data
