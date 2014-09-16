ac6895d58034a2f4f9710c8434622228a479912d
Tom Evans <thomas.jude.evans@gmail.com>
2014-09-16 16:31:17 +0200

    Add 'gps.extract' option 'simple' TRUE, and na.rm = TRUE for range for axis

--- map_start_source2.R
+++ map_start_source2.R
@@ -2,7 +2,7 @@ source("gps_extract.R")
 
 gps.data <- NULL
 for(i in 1:length(flight.numbers)){
-  x <- gps.extract(trips$device_info_serial[t.f][i],trips$start_time[t.f][i],trips$end_time[t.f][i])
+  x <- gps.extract(trips$device_info_serial[t.f][i],trips$start_time[t.f][i],trips$end_time[t.f][i], simple = TRUE)
   gps.data <- rbind(gps.data, x)  
 }
 
@@ -11,12 +11,13 @@ library(maps)
 
 #plot base map
 # Set map limits
-c.xlim <- range(gps.data$longitude)
+# ?range
+c.xlim <- range(gps.data$longitude, na.rm = TRUE)
 dif    <- c.xlim[2] - c.xlim[1]
 dif    <- dif *.12
 c.xlim <- c((c.xlim[1] - dif), (c.xlim[2] + dif))
 
-c.ylim <- range(gps.data$latitude)
+c.ylim <- range(gps.data$latitude, na.rm = TRUE)
 dif    <- c.ylim[2] - c.ylim[1]
 dif    <- dif *.1
 c.ylim <- c((c.ylim[1] - dif), (c.ylim[2] + dif))
