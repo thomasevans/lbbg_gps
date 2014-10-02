79255311d36d6b024c12fa88fc17d9cdabb2378c
Tom Evans <thomas.jude.evans@gmail.com>
2014-10-02 19:48:12 +0200

    New script to compare area use between years for guillemots

---
+++ guillemot_year_foraging_area_comparison.R
@@ -0,0 +1,28 @@
+# For 2009 and 2014 guillemot GPS data produce
+# raster layers for area use weighted by time in
+# raster tile.
+# Then calculate difference between the two rasters
+# to show how area use differs between the two years.
+# Plot all this and save to a PDF file.
+
+
+
+# Load bathymetric data
+
+
+# Load 2014 data
+
+# Load 2013 data (maybe make new 'temp' table
+# in DB first)
+
+# Filter GPS data by quality criteria + distance
+# from colony
+
+# Produce raster layers for both years using
+# points in raster thing
+
+# Make a difference raster, by subtracting one
+# from the other
+
+
+# Plot all of the above on a map
