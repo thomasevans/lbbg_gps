48a5992859baab0d38a50d0cf1b7a0e0e2e13371
Tom Evans <thomas.jude.evans@gmail.com>
2014-10-03 10:57:43 +0200

    Script based on 'guillemot_year_foraging_area_comparison.R' Cleaned up to share with Jonas

---
+++ guillemot_rasterize_gps_bath.R
@@ -0,0 +1,16 @@
+
+# Required files -----
+# You should have the following files in the folder/ directory
+# from which you will work.
+# ...
+
+
+# Set working-directory -----
+# Set working-directory to where above files are located
+
+
+# Load in required files ----
+# Bathymetry
+
+# 2014 GPS data
+
