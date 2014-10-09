d310cdc0c9a7d86119b8cd4d457f7537490b5c37
Tom Evans <thomas.jude.evans@gmail.com>
2014-10-09 12:37:19 +0200

    Import required data

--- guillemot_2009_combine_dive_bout_gps.R
+++ guillemot_2009_combine_dive_bout_gps.R
@@ -1,1 +1,34 @@
 # Analysis of dive-bouts from 2009 guillemot GPS + TDR study
+
+
+
+# Import required data ------
+
+# From DB:
+library("RODBC")
+gps.db <- odbcConnectAccess2007('D:/Dropbox/tracking_db/GPS_db.accdb')
+
+# Dive bouts
+dive.bouts <- sqlQuery(gps.db,
+                        query = "SELECT DISTINCT d.*
+          FROM guillemots_dive_bouts_2009 AS d
+          ORDER BY d.ring_number ASC, d.date_time_start ASC;",
+                        as.is = TRUE)
+
+# GPS data
+gps.points <- sqlQuery(gps.db,
+                       query = "SELECT DISTINCT d.*
+          FROM guillemots_gps_points_igu_2009 AS d
+          ORDER BY d.ring_number ASC, d.date_time ASC;",
+                       as.is = TRUE)
+
+# Bathymetry
+load('bsbd_raster.RData')
+
+
+# Combine two data - for loop to go through all bouts -----
+
+
+
+
+
