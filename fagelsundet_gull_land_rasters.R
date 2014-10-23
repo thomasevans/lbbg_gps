22c63b433e5ec828052765f48930752f72896e17
Tom Evans <thomas.jude.evans@gmail.com>
2014-10-23 23:01:13 +0200

    Wrapped raster production into function (accepts device_id) + few other bits

--- fagelsundet_gull_land_rasters.R
+++ fagelsundet_gull_land_rasters.R
@@ -130,9 +130,11 @@ time.weight.hg.all.for <- 100*((time_interval_narm[f]) /
   (sum(time_interval_narm[f])))
 
 head(time.weight.hg.all.for)
-hist(time.weight.hg.all.for)
+# hist(time.weight.hg.all.for*100)
 
 # Make raster layers -----
+data(SAGA_pal)
+source("color_legend_png_fun.R")
 
 # All 'foraging' locations
 point_raster_forage <- rasterize(obs.xy.for,
@@ -141,60 +143,130 @@ point_raster_forage <- rasterize(obs.xy.for,
                                  fun = sum)
 
 range(values(point_raster_forage), na.rm = TRUE)
-hist(values(point_raster_forage), breaks = 100)
 hist(values(point_raster_forage), breaks = 100,
-     ylim = c(0,50))
+     col = "dark grey")
+hist(values(point_raster_forage), breaks = 100,
+     ylim = c(0,50), col = "dark grey")
 
+values(point_raster_forage) <- log10(values(point_raster_forage))
 
-
-plotKML(point_raster_forage, file = "point_raster_forage_percent.kml",
-        min.png.width = (10*612))
+plotKML(point_raster_forage, file = "raster_hg_all.kml",
+        min.png.width = (20*612),
+        colour_scale = SAGA_pal[[1]])
 # sort(time.weight.hg.all.for, decreasing = TRUE)[1:100]
 
+# Improve colour scale
+10^(range(values(point_raster_forage),na.rm = TRUE))
+
+color_legend_png(legend.file = "layer_legend.png",
+                 ras.val = values(point_raster_forage),
+                 zval = c(0.001,0.01,0.1,1,5,15
+                 ), dig = 3)
 
 # Top 100
 val.100 <- sort(values(point_raster_forage), decreasing = TRUE)[100]
 point_raster_forage_100 <- point_raster_forage
 point_raster_forage_100[values(point_raster_forage) < val.100] <- NA
 
-source("color_legend_png_fun.R")
 
-values(point_raster_forage_100) <- log10(values(point_raster_forage_100))
-data(SAGA_pal)
+
 
 plotKML(point_raster_forage_100, file = "point_raster_forage_percent_100.kml",
-        min.png.width = (10*612), colour_scale = SAGA_pal[[1]])
+        min.png.width = (20*612), colour_scale = SAGA_pal[[1]])
 
 10^(range(values(point_raster_forage_100),na.rm = TRUE))
 # Improve colour scale
 color_legend_png(legend.file = "layer_legend.png",
                  ras.val = values(point_raster_forage_100),
                  zval = c(0.2,0.3,0.5,1,2,5,10,15
-                          ))
+                          ), dig = 1)
 
 hist(values(10^point_raster_forage_100), breaks = 40)
 sum(10^values(point_raster_forage_100), na.rm = TRUE)
 
 
-# 6120 HG example -----
+# 6120 HG -----
 f2 <- (speed < 4) & (col.dist > 1000) & hg.points$device_info_serial == "6120"
 f2[is.na(f2)] <- FALSE
 # summary(f2)
 obs.xy.for.6120 <- obs.xy[f2,]
-a <- rep(1, length(obs.xy.for.6120[,1]))
-# str(a)
-point_raster_forage_6120 <- rasterize(obs.xy.for.6120,
-                                      base_raster,
-                                      fun=function(x, ...) {length(unique(na.omit(x)))})
 
-plotKML(point_raster_forage_6120, file = "point_raster_forage_6120.kml",
-        min.png.width = (10*612))
+time.weight <- 100*((time_interval_narm[f2]) /
+                                 (sum(time_interval_narm[f2])))
 
-point_raster_forage_6120_log <- point_raster_forage_6120
-values(point_raster_forage_6120_log) <- log(values(point_raster_forage_6120_log))
+raster_6120 <- rasterize(obs.xy.for.6120,
+                                 base_raster,
+                                 time.weight,
+                                 fun = sum)
 
-plotKML(point_raster_forage_6120_log, file = "point_raster_forage_6120_log.kml",
-        min.png.width = (10*612))
+
+values(raster_6120) <- log10(values(raster_6120))
+
+plotKML(raster_6120, file = "raster_hg_6120_all.kml",
+        min.png.width = (20*612),
+        colour_scale = SAGA_pal[[1]])
+# sort(time.weight.hg.all.for, decreasing = TRUE)[1:100]
+
+# Improve colour scale
+10^(range(values(raster_6120),na.rm = TRUE))
+
+color_legend_png(legend.file = "layer_legend.png",
+                 ras.val = values(raster_6120),
+                 zval = c(0.001,0.01,0.1,1,5,12
+                 ), dig = 3)
+
+hist(values(10^raster_6120), breaks = 40, ylim = c(0,50),
+     col = "dark grey")
+
+
+
+# function wrap ------
+make.raster <- function(device_id){
+    f2 <- (speed < 4) & (col.dist > 1000) & hg.points$device_info_serial == device_id
+    f2[is.na(f2)] <- FALSE
+    # summary(f2)
+    obs.xy.for.x <- obs.xy[f2,]
+    
+    time.weight <- 100*((time_interval_narm[f2]) /
+                          (sum(time_interval_narm[f2])))
+    
+    raster_x <- rasterize(obs.xy.for.x,
+                             base_raster,
+                             time.weight,
+                             fun = sum)
+    
+    
+    values(raster_x) <- log10(values(raster_x))
+    ?plotKML
+    plotKML(raster_x, file = paste("raster_hg_", device_id, "_all.kml", sep = ""),
+            min.png.width = (20*612),
+            colour_scale = SAGA_pal[[1]])
+    # sort(time.weight.hg.all.for, decreasing = TRUE)[1:100]
+    
+    # Improve colour scale
+#     10^(range(values(raster_x),na.rm = TRUE))
+    top.val <- floor(10^max(values(raster_x),na.rm = TRUE))
+    
+    color_legend_png(legend.file = "layer_legend.png",
+                     ras.val = values(raster_x),
+                     zval = c(0.001,0.01,0.1,1,5,top.val
+                     ), dig = 3)
+    
+    png(paste("raster_hg_", device_id, "_all.png", sep = ""))
+    hist(values(10^raster_x), breaks = 40, ylim = c(0,50),
+         col = "dark grey", main = paste("Bird ID: ",
+                                         device_id, sep = "")
+    )
+         dev.off()
+}    
+
+# Do for each HG -----
+make.raster(6123)
+make.raster(6128)
+make.raster(6149)
+make.raster(6150)
+make.raster(6151)
+make.raster(6156)
 
 
 # Output raster layers to kml files -----
