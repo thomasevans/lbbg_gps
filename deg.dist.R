7d592682eaf63cee10a5ff91bc198e0a9629ed20
Tom Evans <thomas.jude.evans@gmail.com>
2014-10-24 14:45:04 +0200

    Added option to get distance in metres, defaults to km for compatibility with where this function is called in other scripts

--- deg.dist.R
+++ deg.dist.R
@@ -1,7 +1,8 @@
 # Function to calculate grand-circle distance. Copied from package 'fossil'. As this is the only function required from this package, it seems better to include it in a single script.
 # Vavrek (2012) 'fossil' v0.3.7. Palaeoecological and Palaeogeographical Analysis Tools. http://matthewvavrek.com/programs-and-code/fossil/
-deg.dist <- function (long1, lat1, long2, lat2) 
+deg.dist <- function (long1, lat1, long2, lat2, km = TRUE) 
 {
+  # Returns distance in km. Metres if km is false
   rad <- pi/180
   a1 <- lat1 * rad
   a2 <- long1 * rad
@@ -13,5 +14,7 @@ deg.dist <- function (long1, lat1, long2, lat2)
   c <- 2 * atan2(sqrt(a), sqrt(1 - a))
   R <- 40041.47/(2 * pi)
   d <- R * c
+#   if(km == FALSE){d <- (d*1000)}
+  
   return(d)
 }
