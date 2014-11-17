347b1f37ab63690fa123d9931cf4449989b29db5
Tom Evans <thomas.jude.evans@gmail.com>
2014-11-17 11:08:39 +0100

    New script to calculate paramaters for Fågelsundet gull data

---
+++ fagelsundet_01_processing.R
@@ -0,0 +1,12 @@
+# Calculate various paramaters for the GPS tracking data for
+# all Fågelsundet gulls for 2014
+# Including:
+# - Distance from nest
+# - Whether on foraging trip (distance from nest threshhold)
+# - Type of point ('flight', 'non-flight') using speed threshold
+# - Distance to nearest coast
+# - Whether on land/ sea
+# - Combined, signed distance to coast
+# Then output this data to DB with combined primary key of
+# device_info_serial and date_time
+
