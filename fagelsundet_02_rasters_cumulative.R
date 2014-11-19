4a3bb3a109d606dfddafb1c5bbf257371409bc99
Tom Evans <thomas.jude.evans@gmail.com>
2014-11-19 15:16:29 +0100

    Script for raster calculation + cumulative % + cumulative % by time period. Based on script fagelsundet_gull_land_rasters,but more organised

---
+++ fagelsundet_02_rasters_cumulative.R
@@ -0,0 +1,6 @@
+# Make rasters for different individual gulls, and species
+# Ouput to KML files
+# Make histogrammes to show distribution of extensive
+# vs. spatially restricted foraging
+# Calculate cumulative % things as before, then plot
+# Calculate cumulative % thing by time periods, then plot.
