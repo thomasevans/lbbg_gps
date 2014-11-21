a4c29f71538b105a73cbee6fd4d8cbda20538c5c
Tom Evans <thomas.jude.evans@gmail.com>
2014-11-21 17:00:19 +0100

    Simplified to produce raster objects only, not output to KML in the same step. Then cumulative % thing, and plots of this by time period and species

--- fagelsundet_02_rasters_cumulative.R
+++ fagelsundet_02_rasters_cumulative.R
@@ -165,16 +165,14 @@ for(i in 1:4){
 period.names <- c("May 2", "June 1", "June 2", "July 1")
 
 names(gps.2014)
+
+
 # Produce rasters ------
-
-
-# Need to de-bug!!!!
-#
-make.raster <- function(filter, file.name, gps.data = gps.2014, base_raster = base_raster){
-  require("plotKML")
-  data(SAGA_pal)
-  source("color_legend_png_fun.R")
-  gps.f <- gps.data[filter,]
+make.raster <- function(fx, gps.data = gps.2014, base_raster = base_raster){
+ 
+  require("raster")
+  
+  gps.f <- gps.data[fx,]
   
   obs.xy <- cbind(gps.f$longitude, gps.f$latitude)
   
@@ -196,50 +194,606 @@ make.raster <- function(filter, file.name, gps.data = gps.2014, base_raster = ba
                         base_raster,
                         time.weight,
                         fun = sum)
-  
-  
-  values(raster_x) <- log10(values(raster_x))
-
-  plotKML(raster_x, file = paste(file.name, ".kml", sep = ""),
-          min.png.width = (20*612),
-          colour_scale = SAGA_pal[[1]])
-  # sort(time.weight.hg.all.for, decreasing = TRUE)[1:100]
-  
-  # Improve colour scale
-  #     10^(range(values(raster_x),na.rm = TRUE))
-  top.val <- floor(10^max(values(raster_x),na.rm = TRUE))
-  
-  color_legend_png(legend.file = "layer_legend.png",
-                   ras.val = values(raster_x),
-                   zval = c(0.001,0.01,0.1,1,5,top.val
-                   ), dig = 3)
-  
-  png(paste(file.name, "_hist", ".png", sep = ""))
-  hist(values(10^raster_x), breaks = 40, ylim = c(0,50),
-       col = "dark grey", main = file.name
-  )
-  dev.off()
+  return(raster_x)
 }   
 
 
-make.raster(filter = gg.filter, file.name = "GBBG_all")
+# For each bird for each period
+
+n <- length(devices_unique_df$device_info_serial)
+
+# All periods
+ras_birds <- list()
+for(i in 1:n){
+  f1 <- filter_devices[[i]]
+  ras_birds[i] <- make.raster(fx = f1,
+                              gps.data = gps.2014,
+                              base_raster = base_raster)
+}
+
+# Period # 1
+ras_birds_per_01 <- list()
+for(i in 1:n){
+  f1 <- (filter_devices[[i]] & period.filters[[1]])
+  if(sum(f1, na.rm = TRUE) < 1){ras_birds_per_01[i] <- NA} else {
+    ras_birds_per_01[i] <- make.raster(fx = f1,
+                                       gps.data = gps.2014,
+                                       base_raster = base_raster)
+  }  
+}
+
+# Period # 2
+ras_birds_per_02 <- list()
+for(i in 1:n){
+  f1 <- (filter_devices[[i]] & period.filters[[2]])
+  if(sum(f1, na.rm = TRUE) < 1){ras_birds_per_02[i] <- NA} else {
+    ras_birds_per_02[i] <- make.raster(fx = f1,
+                                       gps.data = gps.2014,
+                                       base_raster = base_raster)
+  }  
+}
+
+# Period # 3
+ras_birds_per_03 <- list()
+for(i in 1:n){
+  f1 <- (filter_devices[[i]] & period.filters[[3]])
+  if(sum(f1, na.rm = TRUE) < 1){ras_birds_per_03[i] <- NA} else {
+    ras_birds_per_03[i] <- make.raster(fx = f1,
+                                       gps.data = gps.2014,
+                                       base_raster = base_raster)
+  }  
+}
+
+# Period # 4
+ras_birds_per_04 <- list()
+for(i in 1:n){
+  f1 <- (filter_devices[[i]] & period.filters[[4]])
+  if(sum(f1, na.rm = TRUE) < 1){ras_birds_per_04[i] <- NA} else {
+    ras_birds_per_04[i] <- make.raster(fx = f1,
+                                       gps.data = gps.2014,
+                                       base_raster = base_raster)
+  }  
+}
+
+
+
+# Output rasters to KML files ------
+# Take previously produced raster files and output to KML files
+# for viewing in Google Earth.
 
 
 # Cumulative % by period and individual -------
 
 # Function to get cumulative percentages
-# Take in lat-long data and time intervals
-# Output ordered vector of cell values
+# Take in rasters
+# Output cumulative values
+raster.cumsum.fun <- function(x){
+  if(length(x) != 642580){return(NA)}else{
+    val.sort <- sort(values(x), decreasing = TRUE)
+    return(cumsum(val.sort))
+  }
+}
 
 
 # Loop through all individuals
 # For each individual, and each period within individual run
 # the cumulative % thing and output to a list
 
+cumsum_period_all <- list()
+
+for(i in 1:n){
+  cumsum_period_all[i] <- list(raster.cumsum.fun(ras_birds[[i]]))
+}
+
+cumsum_period_01 <- list()
+
+for(i in 1:n){
+  cumsum_period_01[i] <- list(raster.cumsum.fun(ras_birds_per_01[[i]]))
+}
+
+
+cumsum_period_02 <- list()
+
+for(i in 1:n){
+  cumsum_period_02[i] <- list(raster.cumsum.fun(ras_birds_per_02[[i]]))
+}
+
+
+cumsum_period_03 <- list()
+
+for(i in 1:n){
+  cumsum_period_03[i] <- list(raster.cumsum.fun(ras_birds_per_03[[i]]))
+}
+
+cumsum_period_04 <- list()
+
+for(i in 1:n){
+  cumsum_period_04[i] <- list(raster.cumsum.fun(ras_birds_per_04[[i]]))
+}
+
 
 # Plots for cumulative % thing ------
 
+# Code for transparent colours ----
+# Code from https://github.com/mylesmharrison/colorRampPaletteAlpha/blob/master/colorRampPaletteAlpha.R
+# Hight-lighted by blog post: http://www.everydayanalytics.ca/2014/03/colorRampPalette-alpha-in-R.html
+addalpha <- function(colors, alpha = 1.0) {
+  r <- col2rgb(colors, alpha=T)
+  # Apply alpha
+  r[4,] <- alpha*255
+  r <- r/255.0
+  return(rgb(r[1,], r[2,], r[3,], r[4,]))
+}
+
+
+# GBBG figure -----
+png("GBBG_cumsum.png",  width = 2000, height = 1200, res = 200)
+par(mfrow = c(3,2))
+
+ob.num <- c(1:length(cumsum_period_all[[ggs[1]]]))
+
+plot(cumsum_period_all[[ggs[1]]]~ ob.num, ylim = c(0,100),
+     xlim = c(0,50),
+     ylab = "Foraging time (%)",
+     xlab = "Number of grid squares",
+     las = 1,
+     type = "n",
+     main = "May 15 - July 15")
+
+# ?rainbow
+cols <- rainbow(length(ggs))
+col.obs.transp <- addalpha(cols, alpha = 0.50)
+
+for(i in 1:length(ggs)){
+  ob.num <- c(1:length(cumsum_period_all[[ggs[i]]]))
+  x <- c(0,ob.num)
+  y <- c(0,cumsum_period_all[[ggs[i]]])
+  colx <- cols[i]
+  lines( y ~ x ,
+         type = "l",
+         col = col.obs.transp[i],
+lwd = 2, abline(h = 75, lwd = 2, lty = 2)  ) 
+}
+
+
+
+ob.num <- c(1:length(cumsum_period_01[[ggs[1]]]))
+
+plot(cumsum_period_01[[ggs[1]]]~ ob.num, ylim = c(0,100),
+     xlim = c(0,50),
+     ylab = "Foraging time (%)",
+     xlab = "Number of grid squares",
+     las = 1,
+     type = "n",
+     add = FALSE,
+     main = "May - 2")
+
+# ?rainbow
+cols <- rainbow(length(ggs))
+col.obs.transp <- addalpha(cols, alpha = 0.50)
+
+for(i in 1:length(ggs)){
+  ob.num <- c(1:length(cumsum_period_01[[ggs[i]]]))
+  x <- c(0,ob.num)
+  y <- c(0,cumsum_period_01[[ggs[i]]])
+  colx <- cols[i]
+  lines( y ~ x ,
+         type = "l",
+         col = col.obs.transp[i],
+lwd = 2, abline(h = 75, lwd = 2, lty = 2)  ) 
+}
+
+ob.num <- c(1:length(cumsum_period_02[[ggs[1]]]))
+
+plot(cumsum_period_02[[ggs[1]]]~ ob.num, ylim = c(0,100),
+     xlim = c(0,50),
+     ylab = "Foraging time (%)",
+     xlab = "Number of grid squares",
+     las = 1,
+     type = "n",
+     add = FALSE,
+     main = "June - 1")
+
+# ?rainbow
+cols <- rainbow(length(ggs))
+col.obs.transp <- addalpha(cols, alpha = 0.50)
+
+for(i in 1:length(ggs)){
+  ob.num <- c(1:length(cumsum_period_02[[ggs[i]]]))
+  x <- c(0,ob.num)
+  y <- c(0,cumsum_period_02[[ggs[i]]])
+  colx <- cols[i]
+  lines( y ~ x ,
+         type = "l",
+         col = col.obs.transp[i],
+lwd = 2, abline(h = 75, lwd = 2, lty = 2)  ) 
+}
+
+
+ob.num <- c(1:length(cumsum_period_03[[ggs[1]]]))
+
+plot(cumsum_period_03[[ggs[1]]]~ ob.num, ylim = c(0,100),
+     xlim = c(0,50),
+     ylab = "Foraging time (%)",
+     xlab = "Number of grid squares",
+     las = 1,
+     type = "n",
+     add = FALSE,
+     main = "June - 2")
+
+# ?rainbow
+cols <- rainbow(length(ggs))
+col.obs.transp <- addalpha(cols, alpha = 0.50)
+
+for(i in 1:length(ggs)){
+  ob.num <- c(1:length(cumsum_period_03[[ggs[i]]]))
+  x <- c(0,ob.num)
+  y <- c(0,cumsum_period_03[[ggs[i]]])
+  colx <- cols[i]
+  lines( y ~ x ,
+         type = "l",
+         col = col.obs.transp[i],
+lwd = 2, abline(h = 75, lwd = 2, lty = 2)  ) 
+}
 
 
 
 
+ob.num <- c(1:length(cumsum_period_04[[ggs[1]]]))
+
+plot(cumsum_period_04[[ggs[1]]]~ ob.num, ylim = c(0,100),
+     xlim = c(0,50),
+     ylab = "Foraging time (%)",
+     xlab = "Number of grid squares",
+     las = 1,
+     type = "n",
+     add = FALSE,
+     main = "July - 1")
+
+# ?rainbow
+cols <- rainbow(length(ggs))
+col.obs.transp <- addalpha(cols, alpha = 0.50)
+
+for(i in 1:length(ggs)){
+  ob.num <- c(1:length(cumsum_period_04[[ggs[i]]]))
+  x <- c(0,ob.num)
+  y <- c(0,cumsum_period_04[[ggs[i]]])
+  colx <- cols[i]
+  lines( y ~ x ,
+         type = "l",
+         col = col.obs.transp[i],
+lwd = 2, abline(h = 75, lwd = 2, lty = 2)  ) 
+}
+
+# plot(type = "n")
+
+# legend(x = 30, y = 73,
+#        legend = as.character(devices_unique_df$device_info_serial[ggs]),
+#        #        cex = 0.5,
+#        lty = 1,
+#        col = col.obs.transp,
+#        lwd = 3)
+
+plot(x = c(0:10), y = c(0:10), type="n", axes=F, xlab="", ylab="")
+legend(x = 0, y = 10,
+       legend = as.character(devices_unique_df$device_info_serial[ggs]),
+       #        cex = 0.5,
+       lty = 1,
+       col = col.obs.transp,
+       lwd = 3)
+
+dev.off()
+
+
+
+
+# HG figure -----
+png("HG_cumsum.png",  width = 2000, height = 1200, res = 200)
+par(mfrow = c(3,2))
+
+ob.num <- c(1:length(cumsum_period_all[[hgs[1]]]))
+
+plot(cumsum_period_all[[hgs[1]]]~ ob.num, ylim = c(0,100),
+     xlim = c(0,50),
+     ylab = "Foraging time (%)",
+     xlab = "Number of grid squares",
+     las = 1,
+     type = "n",
+     main = "May 15 - July 15")
+
+# ?rainbow
+cols <- rainbow(length(hgs))
+col.obs.transp <- addalpha(cols, alpha = 0.50)
+
+for(i in 1:length(hgs)){
+  ob.num <- c(1:length(cumsum_period_all[[hgs[i]]]))
+  x <- c(0,ob.num)
+  y <- c(0,cumsum_period_all[[hgs[i]]])
+  colx <- cols[i]
+  lines( y ~ x ,
+         type = "l",
+         col = col.obs.transp[i],
+lwd = 2, abline(h = 75, lwd = 2, lty = 2)  ) 
+}
+
+
+
+ob.num <- c(1:length(cumsum_period_01[[hgs[1]]]))
+
+plot(cumsum_period_01[[hgs[1]]]~ ob.num, ylim = c(0,100),
+     xlim = c(0,50),
+     ylab = "Foraging time (%)",
+     xlab = "Number of grid squares",
+     las = 1,
+     type = "n",
+     add = FALSE,
+     main = "May - 2")
+
+# ?rainbow
+cols <- rainbow(length(hgs))
+col.obs.transp <- addalpha(cols, alpha = 0.50)
+
+for(i in 1:length(hgs)){
+  ob.num <- c(1:length(cumsum_period_01[[hgs[i]]]))
+  x <- c(0,ob.num)
+  y <- c(0,cumsum_period_01[[hgs[i]]])
+  colx <- cols[i]
+  lines( y ~ x ,
+         type = "l",
+         col = col.obs.transp[i],
+lwd = 2, abline(h = 75, lwd = 2, lty = 2)  ) 
+}
+
+ob.num <- c(1:length(cumsum_period_02[[hgs[1]]]))
+
+plot(cumsum_period_02[[hgs[1]]]~ ob.num, ylim = c(0,100),
+     xlim = c(0,50),
+     ylab = "Foraging time (%)",
+     xlab = "Number of grid squares",
+     las = 1,
+     type = "n",
+     add = FALSE,
+     main = "June - 1")
+
+# ?rainbow
+cols <- rainbow(length(hgs))
+col.obs.transp <- addalpha(cols, alpha = 0.50)
+
+for(i in 1:length(hgs)){
+  ob.num <- c(1:length(cumsum_period_02[[hgs[i]]]))
+  x <- c(0,ob.num)
+  y <- c(0,cumsum_period_02[[hgs[i]]])
+  colx <- cols[i]
+  lines( y ~ x ,
+         type = "l",
+         col = col.obs.transp[i],
+lwd = 2, abline(h = 75, lwd = 2, lty = 2)  ) 
+}
+
+
+ob.num <- c(1:length(cumsum_period_03[[hgs[1]]]))
+
+plot(cumsum_period_03[[hgs[1]]]~ ob.num, ylim = c(0,100),
+     xlim = c(0,50),
+     ylab = "Foraging time (%)",
+     xlab = "Number of grid squares",
+     las = 1,
+     type = "n",
+     add = FALSE,
+     main = "June - 2")
+
+# ?rainbow
+cols <- rainbow(length(hgs))
+col.obs.transp <- addalpha(cols, alpha = 0.50)
+
+for(i in 1:length(hgs)){
+  ob.num <- c(1:length(cumsum_period_03[[hgs[i]]]))
+  x <- c(0,ob.num)
+  y <- c(0,cumsum_period_03[[hgs[i]]])
+  colx <- cols[i]
+  lines( y ~ x ,
+         type = "l",
+         col = col.obs.transp[i],
+lwd = 2, abline(h = 75, lwd = 2, lty = 2)  ) 
+}
+
+
+
+
+ob.num <- c(1:length(cumsum_period_04[[hgs[1]]]))
+
+plot(cumsum_period_04[[hgs[1]]]~ ob.num, ylim = c(0,100),
+     xlim = c(0,50),
+     ylab = "Foraging time (%)",
+     xlab = "Number of grid squares",
+     las = 1,
+     type = "n",
+     add = FALSE,
+     main = "July - 1")
+
+# ?rainbow
+cols <- rainbow(length(hgs))
+col.obs.transp <- addalpha(cols, alpha = 0.50)
+
+for(i in 1:length(hgs)){
+  ob.num <- c(1:length(cumsum_period_04[[hgs[i]]]))
+  x <- c(0,ob.num)
+  y <- c(0,cumsum_period_04[[hgs[i]]])
+  colx <- cols[i]
+  lines( y ~ x ,
+         type = "l",
+         col = col.obs.transp[i],
+lwd = 2, abline(h = 75, lwd = 2, lty = 2)  ) 
+}
+
+# plot(type = "n")
+
+# legend(x = 30, y = 73,
+#        legend = as.character(devices_unique_df$device_info_serial[hgs]),
+#        #        cex = 0.5,
+#        lty = 1,
+#        col = col.obs.transp,
+#        lwd = 3)
+
+
+plot(x = c(0:10), y = c(0:10), type="n", axes=F, xlab="", ylab="")
+legend(x = 0, y = 10,
+       legend = as.character(devices_unique_df$device_info_serial[hgs]),
+       #        cex = 0.5,
+       lty = 1,
+       col = col.obs.transp,
+       lwd = 3)
+
+dev.off()
+
+
+
+
+# Figures for CG -----
+png("CG_cumsum.png",  width = 2000, height = 1200, res = 200)
+par(mfrow = c(3,2))
+
+ob.num <- c(1:length(cumsum_period_all[[cgs[1]]]))
+
+plot(cumsum_period_all[[cgs[1]]]~ ob.num, ylim = c(0,100),
+     xlim = c(0,50),
+     ylab = "Foraging time (%)",
+     xlab = "Number of grid squares",
+     las = 1,
+     type = "n",
+     main = "May 15 - July 15")
+
+# ?rainbow
+cols <- rainbow(length(cgs))
+col.obs.transp <- addalpha(cols, alpha = 0.50)
+
+for(i in 1:length(cgs)){
+  ob.num <- c(1:length(cumsum_period_all[[cgs[i]]]))
+  x <- c(0,ob.num)
+  y <- c(0,cumsum_period_all[[cgs[i]]])
+  colx <- cols[i]
+  lines( y ~ x ,
+         type = "l",
+         col = col.obs.transp[i],
+lwd = 2, abline(h = 75, lwd = 2, lty = 2)  ) 
+}
+
+
+
+ob.num <- c(1:length(cumsum_period_01[[cgs[1]]]))
+
+plot(cumsum_period_01[[cgs[1]]]~ ob.num, ylim = c(0,100),
+     xlim = c(0,50),
+     ylab = "Foraging time (%)",
+     xlab = "Number of grid squares",
+     las = 1,
+     type = "n",
+     add = FALSE,
+     main = "May - 2")
+
+# ?rainbow
+cols <- rainbow(length(cgs))
+col.obs.transp <- addalpha(cols, alpha = 0.50)
+
+for(i in 1:length(cgs)){
+  ob.num <- c(1:length(cumsum_period_01[[cgs[i]]]))
+  x <- c(0,ob.num)
+  y <- c(0,cumsum_period_01[[cgs[i]]])
+  colx <- cols[i]
+  lines( y ~ x ,
+         type = "l",
+         col = col.obs.transp[i],
+lwd = 2, abline(h = 75, lwd = 2, lty = 2)  ) 
+}
+
+ob.num <- c(1:length(cumsum_period_02[[cgs[1]]]))
+
+plot(cumsum_period_02[[cgs[1]]]~ ob.num, ylim = c(0,100),
+     xlim = c(0,50),
+     ylab = "Foraging time (%)",
+     xlab = "Number of grid squares",
+     las = 1,
+     type = "n",
+     add = FALSE,
+     main = "June - 1")
+
+# ?rainbow
+cols <- rainbow(length(cgs))
+col.obs.transp <- addalpha(cols, alpha = 0.50)
+
+for(i in 1:length(cgs)){
+  ob.num <- c(1:length(cumsum_period_02[[cgs[i]]]))
+  x <- c(0,ob.num)
+  y <- c(0,cumsum_period_02[[cgs[i]]])
+  colx <- cols[i]
+  lines( y ~ x ,
+         type = "l",
+         col = col.obs.transp[i],
+lwd = 2, abline(h = 75, lwd = 2, lty = 2)  ) 
+}
+
+
+ob.num <- c(1:length(cumsum_period_03[[cgs[1]]]))
+
+plot(cumsum_period_03[[cgs[1]]]~ ob.num, ylim = c(0,100),
+     xlim = c(0,50),
+     ylab = "Foraging time (%)",
+     xlab = "Number of grid squares",
+     las = 1,
+     type = "n",
+     add = FALSE,
+     main = "June - 2")
+
+# ?rainbow
+cols <- rainbow(length(cgs))
+col.obs.transp <- addalpha(cols, alpha = 0.50)
+
+for(i in 1:length(cgs)){
+  ob.num <- c(1:length(cumsum_period_03[[cgs[i]]]))
+  x <- c(0,ob.num)
+  y <- c(0,cumsum_period_03[[cgs[i]]])
+  colx <- cols[i]
+  lines( y ~ x ,
+         type = "l",
+         col = col.obs.transp[i],
+lwd = 2, abline(h = 75, lwd = 2, lty = 2)  ) 
+}
+
+
+
+
+ob.num <- c(1:length(cumsum_period_04[[cgs[1]]]))
+
+plot(cumsum_period_04[[cgs[1]]]~ ob.num, ylim = c(0,100),
+     xlim = c(0,50),
+     ylab = "Foraging time (%)",
+     xlab = "Number of grid squares",
+     las = 1,
+     type = "n",
+     add = FALSE,
+     main = "July - 1")
+
+# ?rainbow
+cols <- rainbow(length(cgs))
+col.obs.transp <- addalpha(cols, alpha = 0.50)
+
+for(i in 1:length(cgs)){
+  ob.num <- c(1:length(cumsum_period_04[[cgs[i]]]))
+  x <- c(0,ob.num)
+  y <- c(0,cumsum_period_04[[cgs[i]]])
+  colx <- cols[i]
+  lines( y ~ x ,
+         type = "l",
+         col = col.obs.transp[i],
+lwd = 2, abline(h = 75, lwd = 2, lty = 2)  ) 
+}
+
+# plot(type = "n")
+plot(x = c(0:10), y = c(0:10), type="n", axes=F, xlab="", ylab="")
+legend(x = 0, y = 10,
+       legend = as.character(devices_unique_df$device_info_serial[cgs]),
+       #        cex = 0.5,
+       lty = 1,
+       col = col.obs.transp,
+       lwd = 3)
+dev.off()
