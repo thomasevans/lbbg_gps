8b354d9caf87849e72e9a1e122b8cb797bea4e32
Tom Evans <thomas.jude.evans@gmail.com>
2014-12-12 11:00:29 +0100

    Script for flight height model

---
+++ LBBG_inward_flights_height_model.R
@@ -0,0 +1,8 @@
+# Developed by Tom Evans at Lund University: tom.evans@biol.lu.se
+# You are welcome to use parts of this code, but please give credit when using it extensively.
+# Code available at https://github.com/thomasevans/lbbg_gps
+
+# Description ------
+# In this script we perform a statistical analysis of the environmental
+# factors determining lesser black-backed gull flight height, focussing
+# principally on meteorological variables.
