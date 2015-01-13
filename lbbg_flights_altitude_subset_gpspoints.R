f645458937af1d609303437b444f59e72949772f
Tom Evans <thomas.jude.evans@gmail.com>
2015-01-13 08:33:15 +0100

    New script to get a subset of GPS points data for inward flights

---
+++ lbbg_flights_altitude_subset_gpspoints.R
@@ -0,0 +1,15 @@
+
+
+# Set working directors to data location
+
+
+# Link to database
+
+# For each flight get GPS points data +/- 15 % time (min 5 minutes)
+# Make DB querry to perform this.
+
+
+# Assemble all the GPS point data (stack)
+
+# Export to csv file
+# Save as RData file, so that field types are preserved.
