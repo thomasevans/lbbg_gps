4661a09303a4da47bdee530671205ef66d06fab1
Tom Evans <thomas.jude.evans@gmail.com>
2015-01-15 11:53:12 +0100

    Quite a few small, mostly formatting/ comment edits

--- lbbg_flights_altitude_figures.R
+++ lbbg_flights_altitude_figures.R
@@ -10,33 +10,29 @@
 setwd("D:/Dropbox/LBBG_flight_height/R_files")
 
 # Load flight information (meta-data)
-flight.details <- read.csv("flight_subset_altitude.csv",
+flight.info <- read.csv("flight_subset_altitude.csv",
                         head = TRUE)
 
 
 #a hack/fix to make the date_time a POSIX object (i.e. R will now recognise this as a date-time object.
-flight.details$start_time <- as.POSIXct(flight.details$start_time, tz="UTC",
+flight.info$start_time <- as.POSIXct(flight.info$start_time, tz="UTC",
                             format = "%Y-%m-%d %H:%M:%S")
-flight.details$end_time <- as.POSIXct(flight.details$end_time, tz="UTC",
+flight.info$end_time <- as.POSIXct(flight.info$end_time, tz="UTC",
                                      format = "%Y-%m-%d %H:%M:%S")
 
 
 # If you wish to view the data run the following:
-fix(flight.details)
+fix(flight.info)
 # Or use 'View', but this might only work in RStudio
-View(flight.details)
+View(flight.info)
 
 
 # Load GPS point data
 load("flight_subset_altitude_points.RData")
 
-summary((points_all$flight_id == flight.id))
-
-
-sample(points_all$flight_id,100)
 
 # For testing (if de-bugging/ modifying the function 'map.flight.id' below)
-flight.id <- 34145
+flight.id <- 23470
 points <- points_all[(points_all$flight_id == flight.id),]
 flight.details <- flight.info[(flight.info$flight_id == flight.id),]
 
@@ -91,18 +87,7 @@ map.flight.id <- function(flight.id = NULL,
   
   col.speed <- cut(speed_2d, breaks = c(0,seq(6,16,2),20,25,100),
                    labels = col.obs.transp)
-  # ?cut
-  # plot(speed_2d, bg = as.character(col.speed),pch = 21, type = "n")
-  # 
-  # # Sybol size vector (based on altitude)
-  # plot(points$altitude, bg = as.character(col.speed),pch = 21, type = "n")
-  # points(points$altitude, bg = as.character(col.speed),
-  #        pch = 21, cex = (1 + ((points$altitude)/20)))
-  # 
-  # 1 + ((points$altitude)/20)
-  # 
-  # plot(points$latitude~points$longitude, bg = as.character(col.speed),
-  #      pch = 21, cex = (0.6 + ((points$altitude)/40)))
+  
   
   point.size.speed <- (0.6 + ((points$altitude)/40))
   
@@ -111,7 +96,6 @@ map.flight.id <- function(flight.id = NULL,
   
   # 4. Make map #######
   
-  #    pdf("test_16641_3.pdf")
   
   # Plot base-map (using axis limits)
   par(mfrow=c(1, 1))
@@ -122,18 +106,17 @@ map.flight.id <- function(flight.id = NULL,
   )
   
   # Add title indicating which flight it is
+  # This is useful if you later want to look at the data for that
+  # specific flight
   title(main = paste("Flight ID:", flight.id))
   
   # Add track lines
-  # Use 'segments', and plot in grey, lwd = 2?
-  # ?segments
   n <- length(points$longitude)
   segments(points$longitude[1:n-1],
            points$latitude[1:n-1],
            points$longitude[2:n],
            points$latitude[2:n],
            lwd = 2,
-           #          col = addalpha("blue", alpha = 0.60))
            col = as.character(col.speed[-1]))
   
   
@@ -142,7 +125,8 @@ map.flight.id <- function(flight.id = NULL,
   # col = "black" (symbol outline)
   # bg = colour vector (fill - speed)
   
-  
+  # Plot every 5th point if GPS log interval is <20 seconds
+  # Otherwise plot all points
   if(flight.details$interval_mean < 20){
     n <- length(points$longitude)
     ind <- c(1:n)
@@ -172,22 +156,11 @@ map.flight.id <- function(flight.id = NULL,
   
   # Add wind arrow
   # Size arrow according to wind speed
-  # Place arrow in centre?
   # Include alpha channel
-  # ?arrow
-  # ?rad
-  # install.packages("circular")
-  library("circular")
+
+  
   library("shape")
-  #   unit.length <- sqrt((x.len*x.len) + (y.len*y.len))/10
-  
-  
-  
-  # map.scale(x = (ax.lim[1]+0.12*x.len),
-  #           y = (ax.lim[3]+0.12*y.len), ratio = FALSE,
-  #           col = "grey50")
-  
-  #   ?Arrows
+
   Arrows(x0 = (ax.lim[1]+0.25*x.len),
          y0 = (ax.lim[3]+0.65*y.len),
          x1 = ((ax.lim[1]+0.25*x.len) +
@@ -197,39 +170,32 @@ map.flight.id <- function(flight.id = NULL,
          lwd = 2,
          arr.type = "simple",
          arr.length = 0.5,
-         col = addalpha("blue", alpha = 0.8)
-         
+         col = addalpha("blue", alpha = 0.8)         
   )
   
+  # Add label to wind arrow giving wind speed
   wind.10m <- sqrt((flight.details$uwnd10m*flight.details$uwnd10m) +
                      (flight.details$vwnd10m*flight.details$vwnd10m))
-  
-  # ?round
-  wind.10m <- round(wind.10m, digits = 2)
-  
-  
+  # Include one decimal place only
+  wind.10m <- round(wind.10m, digits = 1)
+  # Add text and include units
   text(x = (ax.lim[1]+0.27*x.len),
        y = (ax.lim[3]+0.60*y.len),
        labels = bquote(.(wind.10m) ~ ms^-1))
   
-  
-  
-  
-  # ?expression
-  # More tidying...
+  # Add axes labels (for longitude and latitude, and draw a box
+  # around the figure)
   box(col = "grey50", lwd = 2)
   axis(side = (2), las = 1, col = "grey50", col.axis = "grey50")
   axis(side = (1), las = 1, col = "grey50", col.axis = "grey50")
   
   
-  # Make some plots
-  # names(points)
+  # Make some plots to show speed and altitude during flight.
   
+  # For plots of velocity and altitude during trip, make sure that
+  # date-time is recorded as a date-time object.
   date_time <- as.POSIXct(points$date_time, tz = "UTC")
-  
-  # 
-  # y.min <- min(c((speed_2d),points$vdown*5))
-  # y.max <- max(c((speed_2d),points$vdown*5))
+
   
   par(mfrow = c(4,1))
   
@@ -243,8 +209,6 @@ map.flight.id <- function(flight.id = NULL,
        ylim = c(0,max(speed_2d, na.rm = TRUE) + 1),
        axes = FALSE)
   
-  #   ?segments
-  # points$speed_accuracy
   segments(x0 = date_time,
            x1 = date_time,
            y0 = speed_2d - points$speed_accuracy,
@@ -256,15 +220,12 @@ map.flight.id <- function(flight.id = NULL,
   axis(4, las = 1)              # y-axis
   box()
   grid()
-  #   title(main = paste("Flight ID:", flight.id))
   title(ylab = expression("Speed ms"^{-1}), line = 1)
-  #   points(points$vdown*5 ~ date_time,
-  #          col = "red")
+
+  
   
   par(mar=c(0,4,0,4))         # no top spacing
   plot(points$altitude~date_time,
-       #        xlab = "Time",
-       #        ylab = "Height (m)",
        ylab = "",
        ylim = c(-20,max(points$altitude , na.rm = TRUE) +1),
        axes = FALSE)
@@ -284,8 +245,6 @@ map.flight.id <- function(flight.id = NULL,
   
   par(mar=c(0,4,0,4))         # no top spacing
   plot(vh~date_time,
-       #      xlab = "Time",
-       #      ylab = "v/h"
        ylab = "",
        axes = FALSE)
   axis(4,las = 1)              # y-axis
@@ -295,52 +254,13 @@ map.flight.id <- function(flight.id = NULL,
   par(mar=c(3,4,0,4))         
   
   plot(vh~date_time,
-       #      xlab = "Time",
        ylab = "v/h (log scale)",
-       #      ylim = c(-.5,1)
        log = "y",
        las = 1
   )
   grid()
   title(xlab = "Time", line = 2)
   
-  # par(mfrow = c(1,1))
-  # hist(vh[vh > -4 & vh < 4], breaks = 80)
-  
-  
-  # library(ggplot2)
-  # library(reshape2)
-  # str(points)
-  # 
-  # points.sub <- cbind.data.frame(date_time,points$altitude,vh,speed_2d)
-  # names(points.sub) <- c("date_time", "altitude", "vh", "speed_2d")
-  # 
-  # # Use melt to reshape data so values and variables are in separate columns
-  # dt.df <- melt(points.sub, measure.vars = c("altitude", "vh", "speed_2d"))
-  # 
-  # ggplot(dt.df, aes(x = date_time, y = value)) +
-  #   geom_point(aes(color = variable)) +
-  #   facet_grid(variable ~ ., scales = "free_y")
-  #   # Suppress the legend since color isn't actually providing any information
-  # #   opts(legend.position = "none")
-  
-  
-  
-  
-  
-  # ?plot
-  
-  #   names(flight.details)
-  
-  #   abline(h = flight.details$alt_med,
-  #          )
-  
-  
-  
-  
-  
-  
-  #   dev.off()
 }
 
 
@@ -362,8 +282,12 @@ flight.ids <- c(34145, 31321, 22953, 5034,
                 24234, 24611, 16641, 25568,
                 16606, 38576, 38660, 39016)
 
+
+
+
 pdf("flights_different_conditions_graphs_highres2_whole_vh3-log.pdf")
 for(i in 1:length(flight.ids)){
+  
   map.flight.id(flight.id = flight.ids[i])
 }
 dev.off()
